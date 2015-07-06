cfb4bcfacaf96806846eb52981f5cbf0ba7b098d
mikhail.mikhaylov <mikhail.mikhaylov@teamdev.com>
2015-07-06 19:24:24 +0300

    Annotate MessageShould test to be ignored

--- core-java/src/test/java/org/spine3/util/MessagesShould.java
+++ core-java/src/test/java/org/spine3/util/MessagesShould.java
@@ -7,6 +7,7 @@ package org.spine3.util;
 
 import com.google.protobuf.Any;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.spine3.base.UserId;
 
@@ -39,6 +40,7 @@ public class MessagesShould {
     }
 
     @Test
+    @Ignore
     public void convert_from_Any_to_id() {
         UserId result = Messages.fromAny(any);
 
