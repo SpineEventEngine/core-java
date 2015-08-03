2e800e19f41f93f64130ff041afac92c4752e09d
mikhail.mikhaylov <mikhail.mikhaylov@teamdev.com>
2015-08-03 17:31:06 +0300

    Refactor SampleClient.

--- grpc-sample/src/main/java/org/spine3/sample/SampleClient.java
+++ grpc-sample/src/main/java/org/spine3/sample/SampleClient.java
@@ -18,25 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.spine3.sample;/*
- * Copyright 2015, TeamDev Ltd. All rights reserved.
- *
- * Redistribution and use in source and/or binary forms, with or without
- * modification, must retain the above copyright notice and the following
- * disclaimer.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+package org.spine3.sample;
 
 import io.grpc.ChannelImpl;
 import io.grpc.transport.netty.NegotiationType;
@@ -58,11 +40,17 @@ import java.util.concurrent.TimeUnit;
  * {@link org.spine3.sample.server.FileSystemSampleServer}.
  *
  * @author Mikhail Melnik
+ * @author Mikhail Mikhaylov
  */
 public class SampleClient {
 
+    private static final int PORT = 50051;
     private final ChannelImpl channel;
     private final CommandServiceGrpc.CommandServiceBlockingStub blockingStub;
+
+    private static final String RPC_FAILED = "RPC failed";
+    private static final String USER = "User ";
+    private static final String RESULT = "Result: ";
 
     /**
      * Construct client connecting to HelloWorld server at {@code host:port}.
@@ -81,40 +69,37 @@ public class SampleClient {
 
     private void createOrder(UserId userId, OrderId orderId) {
         try {
-            log().info("User " + userId + " tries to create new order " + orderId + " ...");
+            log().info(USER + userId + " tries to create new order " + orderId + " ...");
             CommandRequest request = Requests.createOrder(userId, orderId);
 
             CommandResult result = blockingStub.handle(request);
-            log().info("Result: " + Messages.toText(result));
+            log().info(RESULT + Messages.toText(result));
         } catch (RuntimeException e) {
-            log().warn("RPC failed", e);
-            return;
+            log().warn(RPC_FAILED, e);
         }
     }
 
     private void addOrderLine(UserId userId, OrderId orderId) {
         try {
-            log().info("User " + userId + " tries to add new order line in order " + orderId + " ...");
+            log().info(USER + userId + " tries to add new order line in order " + orderId + " ...");
             CommandRequest request = Requests.addOrderLine(userId, orderId);
 
             CommandResult result = blockingStub.handle(request);
-            log().info("Result: " + Messages.toText(result));
+            log().info(RESULT + Messages.toText(result));
         } catch (RuntimeException e) {
-            log().warn("RPC failed", e);
-            return;
+            log().warn(RPC_FAILED, e);
         }
     }
 
     private void payOrder(UserId userId, OrderId orderId) {
         try {
-            log().info("User " + userId + " tries to pay money for order " + orderId + " ...");
+            log().info(USER + userId + " tries to pay money for order " + orderId + " ...");
             CommandRequest request = Requests.payOrder(userId, orderId);
 
             CommandResult result = blockingStub.handle(request);
-            log().info("Result: " + Messages.toText(result));
+            log().info(RESULT + Messages.toText(result));
         } catch (RuntimeException e) {
-            log().warn("RPC failed", e);
-            return;
+            log().warn(RPC_FAILED, e);
         }
     }
 
@@ -122,10 +107,10 @@ public class SampleClient {
      * Greet server. If provided, the first element of {@code args} is the name to use in the
      * greeting.
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
 
         /* Access a service running on the local machine on port 50051 */
-        SampleClient client = new SampleClient("localhost", 50051);
+        SampleClient client = new SampleClient("localhost", PORT);
 
         try {
 
