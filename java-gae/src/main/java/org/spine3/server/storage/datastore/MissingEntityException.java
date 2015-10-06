9ce4d47fe9612801679ef2d681bc9fc038a18433
Alexander Litus <alexander.litus@teamdev.com>
2015-10-06 17:18:21 +0300

    Remove redundant Datastore storage impl classes.

--- java-gae/src/main/java/org/spine3/server/storage/datastore/MissingEntityException.java
+++ java-gae/src/main/java/org/spine3/server/storage/datastore/MissingEntityException.java
@@ -17,26 +17,28 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.spine3.server.storage.datastore;
 
-package org.spine3.gae.datastore;
-
-import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import com.google.protobuf.Message;
+import org.spine3.util.Commands;
 
 /**
- * A converter of Protobuf messages into DataStore entities.
+ * Exception is thrown if the data store could not read entity by id.
+ * <p>
+ * To be found entity should first be stored into data store.
  *
- * @param <T> a class of messages the converter supports
- * @author Mikhail Mikhaylov
+ * @author Mikhail Meikhaylov
  */
-interface Converter<T extends Message> {
+public class MissingEntityException extends RuntimeException {
 
-    /**
-     * Creates a DataStore entity from the passed Protobuf message.
-     *
-     * @param message source message
-     * @return new entity instance
-     */
-    Entity convert(T message);
+    public MissingEntityException(Key key, Throwable cause) {
+        super(createMessage(key), cause);
+    }
 
+    private static String createMessage(Key key) {
+        return "Unable to find entity by key " + key + " in data store";
+    }
+
+    private static final long serialVersionUID = 0L;
 }
