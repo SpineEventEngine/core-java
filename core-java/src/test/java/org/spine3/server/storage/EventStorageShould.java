da286a6968c507c8071ee3c35dc086eb054fc327
Alexander Litus <alexander.litus@teamdev.com>
2015-10-06 13:52:39 +0300

    Avoid duplication in tests.

--- core-java/src/test/java/org/spine3/server/storage/EventStorageShould.java
+++ core-java/src/test/java/org/spine3/server/storage/EventStorageShould.java
@@ -20,7 +20,6 @@
 
 package org.spine3.server.storage;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.spine3.base.EventContext;
 import org.spine3.base.EventRecord;
@@ -32,7 +31,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.*;
 import static org.spine3.protobuf.Messages.toAny;
+import static org.spine3.util.Events.toEventRecord;
 import static org.spine3.util.Events.toEventRecordsList;
 import static org.spine3.util.testutil.AggregateIdFactory.createCommon;
 import static org.spine3.util.testutil.EventStoreRecordFactory.*;
@@ -51,7 +52,7 @@ public abstract class EventStorageShould {
     public void return_iterator_over_empty_collection_if_read_records_from_empty_storage() {
 
         final Iterator<EventRecord> iterator = storage.allEvents();
-        Assert.assertFalse(iterator.hasNext());
+        assertFalse(iterator.hasNext());
     }
 
     @Test(expected = NullPointerException.class)
@@ -65,33 +66,30 @@ public abstract class EventStorageShould {
         final EventRecord expected = getProjectCreatedRecord();
         storage.store(expected);
 
-        final Iterator<EventRecord> iterator = storage.allEvents();
-
-        Assert.assertTrue(iterator.hasNext());
-
-        final EventRecord actual = iterator.next();
-
-        assertEventRecordsAreEqual(expected, actual);
-
-        Assert.assertFalse(iterator.hasNext());
+        assertStorageContains(expected);
     }
 
     @Test
     public void write_and_read_one_event() {
 
-        final EventStoreRecord expected = projectCreated();
-        storage.write(expected);
+        final EventStoreRecord recordToStore = projectCreated();
+        final EventRecord expected = toEventRecord(recordToStore);
+
+        storage.write(recordToStore);
+
+        assertStorageContains(expected);
+    }
+
+    private void assertStorageContains(EventRecordOrBuilder expected) {
 
         final Iterator<EventRecord> iterator = storage.allEvents();
 
-        Assert.assertTrue(iterator.hasNext());
+        assertTrue(iterator.hasNext());
 
         final EventRecord actual = iterator.next();
 
-        Assert.assertEquals(expected.getEvent(), actual.getEvent());
-        Assert.assertEquals(expected.getContext(), actual.getContext());
-
-        Assert.assertFalse(iterator.hasNext());
+        assertEventRecordsAreEqual(expected, actual);
+        assertFalse(iterator.hasNext());
     }
 
     @Test
@@ -122,7 +120,6 @@ public abstract class EventStorageShould {
 
         final Iterator<EventRecord> iteratorFirst = storage.allEvents();
         final List<EventRecord> actualRecordsFirst = newArrayList(iteratorFirst);
-
         assertEventRecordListsAreEqual(expectedRecords, actualRecordsFirst);
 
         final Iterator<EventRecord> iteratorSecond = storage.allEvents();
@@ -132,7 +129,7 @@ public abstract class EventStorageShould {
 
     private static void assertEventRecordListsAreEqual(List<EventRecord> expected, List<EventRecord> actual) {
         if (expected.size() != actual.size()) {
-            Assert.fail("Expected records count: " + expected.size() + " is not equal to actual records count: " + actual.size());
+            fail("Expected records count: " + expected.size() + " is not equal to actual records count: " + actual.size());
         }
         for (int i = 0; i < expected.size(); i++) {
             assertEventRecordsAreEqual(expected.get(i), actual.get(i));
@@ -140,8 +137,8 @@ public abstract class EventStorageShould {
     }
 
     protected static void assertEventRecordsAreEqual(EventRecordOrBuilder expected, EventRecordOrBuilder actual) {
-        Assert.assertEquals(expected.getEvent(), actual.getEvent());
-        Assert.assertEquals(expected.getContext(), actual.getContext());
+        assertEquals(expected.getEvent(), actual.getEvent());
+        assertEquals(expected.getContext(), actual.getContext());
     }
 
     private static List<EventStoreRecord> createEventStoreRecords() {
