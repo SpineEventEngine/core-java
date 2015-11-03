f2961b32cd52a54ffbd6bc840e9338a5e4412cf5
Alexander Litus <alexander.litus@teamdev.com>
2015-11-03 16:46:01 +0200

    Cover Engine with tests 100%.

--- core-java/src/test/java/org/spine3/server/EngineShould.java
+++ core-java/src/test/java/org/spine3/server/EngineShould.java
@@ -20,12 +20,38 @@
 
 package org.spine3.server;
 
+import com.google.protobuf.Duration;
+import com.google.protobuf.Timestamp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.spine3.base.*;
+import org.spine3.eventbus.EventBus;
+import org.spine3.eventbus.Subscribe;
+import org.spine3.server.aggregate.AggregateRepositoryBase;
+import org.spine3.server.aggregate.AggregateShould;
+import org.spine3.server.aggregate.error.MissingEventApplierException;
+import org.spine3.server.error.UnsupportedCommandException;
 import org.spine3.server.storage.memory.InMemoryStorageFactory;
+import org.spine3.test.project.Project;
+import org.spine3.test.project.ProjectId;
+import org.spine3.test.project.event.ProjectCreated;
+import org.spine3.test.project.event.ProjectStarted;
+import org.spine3.test.project.event.TaskAdded;
+import org.spine3.util.Users;
+import org.spine3.util.testutil.AggregateIdFactory;
 
-import static org.junit.Assert.assertNotNull;
+import javax.annotation.Nonnull;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newLinkedList;
+import static com.google.protobuf.util.TimeUtil.add;
+import static com.google.protobuf.util.TimeUtil.getCurrentTime;
+import static org.junit.Assert.*;
+import static org.spine3.protobuf.Durations.seconds;
+import static org.spine3.protobuf.Messages.fromAny;
+import static org.spine3.util.testutil.CommandRequestFactory.*;
 
 /**
  * @author Alexander Litus
@@ -33,14 +59,25 @@ import static org.junit.Assert.assertNotNull;
 @SuppressWarnings("InstanceMethodNamingConvention")
 public class EngineShould {
 
+    private final UserId userId = Users.newUserId("test_user");
+    private final ProjectId projectId = AggregateIdFactory.newProjectId("test_project_id");
+    private final EventSubscriber eventSubscriber = new EventSubscriber();
+
+    private boolean handlersRegistered = false;
+
     @Before
     public void setUp() {
+
         Engine.start(InMemoryStorageFactory.getInstance());
     }
 
     @After
     public void tearDown() {
+
         Engine.stop();
+        if (handlersRegistered) {
+            EventBus.getInstance().unregister(eventSubscriber);
+        }
     }
 
     @Test(expected = IllegalStateException.class)
@@ -65,11 +102,160 @@ public class EngineShould {
 
     @Test
     public void return_EventBus() {
+
         assertNotNull(Engine.getInstance().getEventBus());
     }
 
     @Test
     public void return_CommandDispatcher() {
+
         assertNotNull(Engine.getInstance().getCommandDispatcher());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throw_exception_if_call_process_with_null_parameter() {
+        // noinspection ConstantConditions
+        Engine.getInstance().process(null);
+    }
+
+    @Test(expected = UnsupportedCommandException.class)
+    public void throw_exception_if_not_register_any_repositories_and_try_to_process_command() {
+
+        Engine.getInstance().process(createProject());
+    }
+
+    @Test(expected = MissingEventApplierException.class)
+    public void throw_exception_if_not_register_any_event_handlers_and_try_to_process_command() {
+
+        final Engine engine = Engine.getInstance();
+        engine.register(new ProjectAggregateRepository());
+
+        engine.process(createProject());
+    }
+
+    @Test
+    public void register_test_repositories_and_handlers() {
+
+        registerAll();
+    }
+
+    @Test
+    public void return_true_if_started() {
+
+        final boolean isStarted = Engine.getInstance().isStarted();
+        assertTrue(isStarted);
+    }
+
+    @Test
+    public void process_one_command_and_return_appropriate_result() {
+
+        registerAll();
+        final CommandRequest request = createProject(userId, projectId, getCurrentTime());
+
+        final CommandResult result = Engine.getInstance().process(request);
+
+        assertCommandResultsAreValid(newArrayList(request), newArrayList(result));
+    }
+
+    @Test
+    public void process_several_commands_and_return_appropriate_results() {
+
+        registerAll();
+        final List<CommandRequest> requests = generateRequests();
+
+        final List<CommandResult> results = processRequests(requests);
+
+        assertCommandResultsAreValid(requests, results);
+    }
+
+    private void assertCommandResultsAreValid(List<CommandRequest> requests, List<CommandResult> results) {
+
+        assertEquals(requests.size(), results.size());
+
+        for (int i = 0; i < requests.size(); i++) {
+            assertRequestAndResultMatch(requests.get(i), results.get(i));
+        }
+    }
+
+    private void assertRequestAndResultMatch(CommandRequestOrBuilder request, CommandResultOrBuilder result) {
+
+        final Timestamp expectedTime = request.getContext().getCommandId().getTimestamp();
+
+        final List<EventRecord> records = result.getEventRecordList();
+        assertEquals(1, records.size());
+        final EventRecord actualRecord = records.get(0);
+        final ProjectId actualProjectId = fromAny(actualRecord.getContext().getAggregateId());
+        final CommandId actualCommandId = actualRecord.getContext().getEventId().getCommandId();
+
+        assertEquals(projectId, actualProjectId);
+        assertEquals(userId, actualCommandId.getActor());
+        assertEquals(expectedTime, actualCommandId.getTimestamp());
+    }
+
+    /**
+     * Registers all test repositories, handlers etc.
+     */
+    private void registerAll() {
+
+        Engine.getInstance().register(new ProjectAggregateRepository());
+        Engine.getInstance().register(new TestEntityRepository());
+        EventBus.getInstance().register(eventSubscriber);
+        handlersRegistered = true;
+    }
+
+    private static List<CommandResult> processRequests(Iterable<CommandRequest> requests) {
+
+        final List<CommandResult> results = newLinkedList();
+        for (CommandRequest request : requests) {
+            final CommandResult result = Engine.getInstance().process(request);
+            results.add(result);
+        }
+        return results;
+    }
+
+    private List<CommandRequest> generateRequests() {
+
+        final Duration delta = seconds(10);
+        final Timestamp time1 = getCurrentTime();
+        final Timestamp time2 = add(time1, delta);
+        final Timestamp time3 = add(time2, delta);
+
+        final CommandRequest createProject = createProject(userId, projectId, time1);
+        final CommandRequest addTask = addTask(userId, projectId, time2);
+        final CommandRequest startProject = startProject(userId, projectId, time3);
+
+        return newArrayList(createProject, addTask, startProject);
+    }
+
+    private static class ProjectAggregateRepository extends AggregateRepositoryBase<ProjectId, AggregateShould.ProjectAggregate> {
+    }
+
+    private static class TestEntityRepository extends EntityRepository<String, TestEntity, Project> {
+    }
+
+    public static class TestEntity extends Entity<String, Project> {
+        public TestEntity(String id) {
+            super(id);
+        }
+        @Nonnull
+        @Override
+        protected Project getDefaultState() {
+            return Project.getDefaultInstance();
+        }
+    }
+
+    private static class EventSubscriber {
+
+        @Subscribe
+        public void on(ProjectCreated event, EventContext context) {
+        }
+
+        @Subscribe
+        public void on(TaskAdded event, EventContext context) {
+        }
+
+        @Subscribe
+        public void on(ProjectStarted event, EventContext context) {
+        }
     }
 }
