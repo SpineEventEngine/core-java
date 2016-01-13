77ac9aa4ef8a5ba8403d60227cc11e94b72fbb51
Alexander Litus <alexander.litus@teamdev.com>
2016-01-13 11:45:38 +0200

    Update copyright to 2016.

--- core-java/src/test/java/org/spine3/testdata/TestContextFactory.java
+++ core-java/src/test/java/org/spine3/testdata/TestContextFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, TeamDev Ltd. All rights reserved.
+ * Copyright 2016, TeamDev Ltd. All rights reserved.
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
