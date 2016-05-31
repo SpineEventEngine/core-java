6e92429fc6ce90d44ff61a0a00c57d1170bdfe06
Alexander Litus <alexander.litus@teamdev.com>
2016-05-31 14:01:48 +0300

    Add a link to an article on Multi-tenant apps to docs.

--- server/src/main/java/org/spine3/server/users/CurrentTenant.java
+++ server/src/main/java/org/spine3/server/users/CurrentTenant.java
@@ -33,6 +33,7 @@ import static org.spine3.validate.Validate.isNotDefault;
  * This class allows to set a tenant when handling a command or a query
  * in a multitenant application.
  *
+ * @see <a href="https://msdn.microsoft.com/en-us/library/aa479086.aspx">Multi-Tenant Data Architecture</a>
  * @author Alexander Yevsyukov
  */
 @SPI
