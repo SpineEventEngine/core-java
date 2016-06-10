5c37a2b93756a9af9c7f27f5907daf4bdf852a63
andrey.lavrov <andrey.lavrov@teamdev.com>
2016-06-10 14:30:13 +0300

    Refactored AggregateShould

--- server/src/test/java/org/spine3/server/aggregate/AggregateShould.java
+++ server/src/test/java/org/spine3/server/aggregate/AggregateShould.java
@@ -33,16 +33,14 @@ import org.spine3.base.Event;
 import org.spine3.base.EventContext;
 import org.spine3.protobuf.Timestamps;
 import org.spine3.server.command.Assign;
-import org.spine3.test.project.Project;
-import org.spine3.test.project.ProjectId;
-import org.spine3.test.project.command.AddTask;
-import org.spine3.test.project.command.CreateProject;
-import org.spine3.test.project.command.StartProject;
-import org.spine3.test.project.event.ProjectCreated;
-import org.spine3.test.project.event.ProjectStarted;
-import org.spine3.test.project.event.TaskAdded;
-import org.spine3.testdata.TestAggregateIdFactory;
-import org.spine3.testdata.TestCommands;
+import org.spine3.test.aggregate.Project;
+import org.spine3.test.aggregate.ProjectId;
+import org.spine3.test.aggregate.command.AddTask;
+import org.spine3.test.aggregate.command.CreateProject;
+import org.spine3.test.aggregate.command.StartProject;
+import org.spine3.test.aggregate.event.ProjectCreated;
+import org.spine3.test.aggregate.event.ProjectStarted;
+import org.spine3.test.aggregate.event.TaskAdded;
 
 import javax.annotation.Nullable;
 import java.util.Collection;
@@ -56,11 +54,9 @@ import static org.junit.Assert.*;
 import static org.spine3.protobuf.Messages.fromAny;
 import static org.spine3.protobuf.Messages.toAny;
 import static org.spine3.test.Tests.currentTimeSeconds;
-import static org.spine3.test.project.Project.newBuilder;
+import static org.spine3.test.aggregate.Project.newBuilder;
 import static org.spine3.testdata.TestCommandContextFactory.createCommandContext;
 import static org.spine3.testdata.TestEventContextFactory.createEventContext;
-import static org.spine3.testdata.TestEventFactory.*;
-import static org.spine3.testdata.TestEventMessageFactory.*;
 
 /**
  * @author Alexander Litus
@@ -68,14 +64,14 @@ import static org.spine3.testdata.TestEventMessageFactory.*;
 @SuppressWarnings({"TypeMayBeWeakened", "InstanceMethodNamingConvention", "ClassWithTooManyMethods"})
 public class AggregateShould {
 
-    private static final ProjectId ID = TestAggregateIdFactory.newProjectId();
+    private static final ProjectId ID = Generate.AggregateId.newProjectId();
 
     private static final CommandContext COMMAND_CONTEXT = createCommandContext();
     private static final EventContext EVENT_CONTEXT = createEventContext(ID);
 
-    private final CreateProject createProject = TestCommands.createProjectMsg(ID);
-    private final AddTask addTask = TestCommands.addTaskMsg(ID);
-    private final StartProject startProject = TestCommands.startProjectMsg(ID);
+    private final CreateProject createProject = Generate.Command.createProjectMsg(ID);
+    private final AddTask addTask = Generate.Command.addTaskMsg(ID);
+    private final StartProject startProject = Generate.Command.startProjectMsg(ID);
 
     private TestAggregate aggregate;
 
@@ -366,9 +362,9 @@ public class AggregateShould {
 
     private static List<Event> getProjectEvents() {
         final List<Event> events = newLinkedList();
-        events.add(projectCreatedEvent(ID, EVENT_CONTEXT));
-        events.add(taskAddedEvent(ID, EVENT_CONTEXT));
-        events.add(projectStartedEvent(ID, EVENT_CONTEXT));
+        events.add(Generate.Event.projectCreatedEvent(ID, EVENT_CONTEXT));
+        events.add(Generate.Event.taskAddedEvent(ID, EVENT_CONTEXT));
+        events.add(Generate.Event.projectStartedEvent(ID, EVENT_CONTEXT));
         return events;
     }
     
@@ -405,19 +401,19 @@ public class AggregateShould {
         @Assign
         public ProjectCreated handle(CreateProject cmd, CommandContext ctx) {
             isCreateProjectCommandHandled = true;
-            return projectCreatedMsg(cmd.getProjectId());
+            return Generate.EventMessage.projectCreatedMsg(cmd.getProjectId());
         }
 
         @Assign
         public TaskAdded handle(AddTask cmd, CommandContext ctx) {
             isAddTaskCommandHandled = true;
-            return taskAddedMsg(cmd.getProjectId());
+            return Generate.EventMessage.taskAddedMsg(cmd.getProjectId());
         }
 
         @Assign
         public List<ProjectStarted> handle(StartProject cmd, CommandContext ctx) {
             isStartProjectCommandHandled = true;
-            final ProjectStarted message = projectStartedMsg(cmd.getProjectId());
+            final ProjectStarted message = Generate.EventMessage.projectStartedMsg(cmd.getProjectId());
             return newArrayList(message);
         }
 
@@ -468,7 +464,7 @@ public class AggregateShould {
         @Assign
         public ProjectCreated handle(CreateProject cmd, CommandContext ctx) {
             isCreateProjectCommandHandled = true;
-            return projectCreatedMsg(cmd.getProjectId());
+            return Generate.EventMessage.projectCreatedMsg(cmd.getProjectId());
         }
     }
 
@@ -546,7 +542,7 @@ public class AggregateShould {
             if (brokenHandler) {
                 throw new IllegalStateException(BROKEN_HANDLER);
             }
-            return projectCreatedMsg(cmd.getProjectId());
+            return Generate.EventMessage.projectCreatedMsg(cmd.getProjectId());
         }
 
         @Apply
@@ -567,7 +563,7 @@ public class AggregateShould {
     public void propagate_RuntimeException_when_handler_throws() {
         final FaultyAggregate faultyAggregate = new FaultyAggregate(ID, true, false);
 
-        final Command command = TestCommands.createProjectCmd();
+        final Command command = Generate.Command.createProjectCmd();
         try {
             faultyAggregate.dispatchForTest(command.getMessage(), command.getContext());
         } catch (RuntimeException e) {
@@ -582,7 +578,7 @@ public class AggregateShould {
     public void propagate_RuntimeException_when_applier_throws() {
         final FaultyAggregate faultyAggregate = new FaultyAggregate(ID, false, true);
 
-        final Command command = TestCommands.createProjectCmd();
+        final Command command = Generate.Command.createProjectCmd();
         try {
             faultyAggregate.dispatchForTest(command.getMessage(), command.getContext());
         } catch (RuntimeException e) {
@@ -597,7 +593,7 @@ public class AggregateShould {
     public void propagate_RuntimeException_when_play_raises_exception() {
         final FaultyAggregate faultyAggregate = new FaultyAggregate(ID, false, true);
         try {
-            faultyAggregate.play(ImmutableList.of(projectCreatedEvent()));
+            faultyAggregate.play(ImmutableList.of(Generate.Event.projectCreatedEvent()));
         } catch (RuntimeException e) {
             @SuppressWarnings("ThrowableResultOfMethodCallIgnored") // ... because we need it for checking.
             final Throwable cause = Throwables.getRootCause(e);
--- server/src/test/java/org/spine3/server/aggregate/Generate.java
+++ server/src/test/java/org/spine3/server/aggregate/Generate.java
@@ -19,13 +19,36 @@
  */
 
 package org.spine3.server.aggregate;
+import static com.google.protobuf.util.TimeUtil.getCurrentTime;
 
+import com.google.protobuf.Message;
+import com.google.protobuf.Timestamp;
+import org.spine3.base.Command;
+import org.spine3.base.CommandContext;
+import org.spine3.base.CommandId;
+import org.spine3.base.Commands;
+import org.spine3.base.Event;
+import org.spine3.base.EventContext;
+
+import static com.google.protobuf.util.TimeUtil.getCurrentTime;
+import static org.spine3.base.Identifiers.newUuid;
+import static org.spine3.client.UserUtil.newUserId;
+import static org.spine3.protobuf.Messages.toAny;
+import static org.spine3.testdata.TestAggregateIdFactory.newProjectId;
+import static org.spine3.testdata.TestCommandContextFactory.createCommandContext;
+import static org.spine3.testdata.TestEventContextFactory.createEventContext;
 
 import org.spine3.test.aggregate.ProjectId;
+
 import org.spine3.test.aggregate.event.ProjectCreated;
 import org.spine3.test.aggregate.event.TaskAdded;
+import org.spine3.test.aggregate.event.ProjectStarted;
 
-import static org.spine3.base.Identifiers.newUuid;
+import org.spine3.test.aggregate.command.CreateProject;
+import org.spine3.test.aggregate.command.AddTask;
+import org.spine3.test.aggregate.command.StartProject;
+import org.spine3.users.UserId;
+
 
 public class Generate {
     public static class AggregateId {
@@ -62,6 +85,128 @@ public class Generate {
 
         public static TaskAdded taskAddedMsg(ProjectId id) {
             return TaskAdded.newBuilder().setProjectId(id).build();
+        }
+
+        public static ProjectStarted projectStartedMsg(ProjectId id) {
+            return ProjectStarted.newBuilder().setProjectId(id).build();
+        }
+    }
+
+
+    public static class Command{
+
+        private static final UserId USER_ID = newUserId(newUuid());
+        private static final ProjectId PROJECT_ID = Generate.AggregateId.newProjectId();
+
+        private Command() {
+        }
+
+        /**
+         * Creates a new {@link CreateProject} command with the given project ID.
+         */
+        public static CreateProject createProjectMsg(ProjectId id) {
+            return CreateProject.newBuilder().setProjectId(id).build();
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Command} with default properties (current time etc).
+         */
+        public static org.spine3.base.Command createProjectCmd() {
+            return createProjectCmd(getCurrentTime());
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Command} with the given timestamp.
+         */
+        public static org.spine3.base.Command createProjectCmd(Timestamp when) {
+            return createProjectCmd(USER_ID, PROJECT_ID, when);
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Command} with the given userId, projectId and timestamp.
+         */
+        public static org.spine3.base.Command createProjectCmd(UserId userId, ProjectId projectId, Timestamp when) {
+            final CreateProject command = createProjectMsg(projectId);
+            return createCommand(command, userId, when);
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Command} with the given command, userId and timestamp using default
+         * {@link CommandId} instance.
+         */
+        public static org.spine3.base.Command createCommand(Message command, UserId userId, Timestamp when) {
+            final CommandContext context = createCommandContext(userId, Commands.generateId(), when);
+            final org.spine3.base.Command result = Commands.create(command, context);
+            return result;
+        }
+
+        /**
+         * Creates a new {@link AddTask} command with the given project ID.
+         */
+        public static AddTask addTaskMsg(ProjectId id) {
+            return AddTask.newBuilder().setProjectId(id).build();
+        }
+
+        /**
+         * Creates a new {@link StartProject} command with the given project ID.
+         */
+        public static StartProject startProjectMsg(ProjectId id) {
+            return StartProject.newBuilder().setProjectId(id).build();
+        }
+    }
+
+
+    public static class Event{
+        private static final ProjectId PROJECT_ID = Generate.AggregateId.newProjectId();
+
+        private Event() {
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Event} with default properties.
+         */
+        public static org.spine3.base.Event projectCreatedEvent() {
+            return projectCreatedEvent(PROJECT_ID);
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Event} with the given projectId.
+         */
+        public static org.spine3.base.Event projectCreatedEvent(ProjectId projectId) {
+            return projectCreatedEvent(projectId, createEventContext(projectId));
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Event} with the given projectId and eventContext.
+         */
+        public static org.spine3.base.Event projectCreatedEvent(ProjectId projectId, EventContext eventContext) {
+            final ProjectCreated eventMessage = Generate.EventMessage.projectCreatedMsg(projectId);
+            final org.spine3.base.Event.Builder builder = org.spine3.base.Event.newBuilder()
+                                                                               .setContext(eventContext)
+                                                                               .setMessage(toAny(eventMessage));
+            return builder.build();
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Event} with the given projectId and eventContext.
+         */
+        public static org.spine3.base.Event taskAddedEvent(ProjectId projectId, EventContext eventContext) {
+            final TaskAdded eventMessage = Generate.EventMessage.taskAddedMsg(projectId);
+            final org.spine3.base.Event.Builder builder = org.spine3.base.Event.newBuilder()
+                                                                               .setContext(eventContext)
+                                                                               .setMessage(toAny(eventMessage));
+            return builder.build();
+        }
+
+        /**
+         * Creates a new {@link org.spine3.base.Event} with the given projectId and eventContext.
+         */
+        public static org.spine3.base.Event projectStartedEvent(ProjectId projectId, EventContext eventContext) {
+            final ProjectStarted eventMessage = Generate.EventMessage.projectStartedMsg(projectId);
+            final org.spine3.base.Event.Builder builder = org.spine3.base.Event.newBuilder()
+                                                                               .setContext(eventContext)
+                                                                               .setMessage(toAny(eventMessage));
+            return builder.build();
         }
     }
 }
