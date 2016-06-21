516550b7f93077c73036a8e938be9f49dada2785
Alexander Litus <alexander.litus@teamdev.com>
2016-06-21 15:48:02 +0300

    Move TestsShould to an appropriate module.

--- testutil/src/test/java/org/spine3/test/TestsShould.java
+++ testutil/src/test/java/org/spine3/test/TestsShould.java
@@ -18,13 +18,12 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.spine3.util;
+package org.spine3.test;
 
 import com.google.protobuf.Timestamp;
 import org.junit.Test;
 import org.spine3.protobuf.Durations;
 import org.spine3.protobuf.Timestamps;
-import org.spine3.test.Tests;
 
 import static com.google.protobuf.util.TimeUtil.subtract;
 import static org.junit.Assert.*;
