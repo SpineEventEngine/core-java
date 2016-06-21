f03b4a4b6d8d19be8c4ae7f0432109139016e7b0
Alexander Litus <alexander.litus@teamdev.com>
2016-06-21 17:13:51 +0300

    Add TypeName tests.

--- client/src/test/java/org/spine3/type/TypeNameShould.java
+++ client/src/test/java/org/spine3/type/TypeNameShould.java
@@ -23,7 +23,6 @@ package org.spine3.type;
 import com.google.protobuf.Any;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Message;
 import com.google.protobuf.StringValue;
 import com.google.protobuf.UInt64Value;
 import org.junit.Test;
@@ -39,13 +38,15 @@ import static org.spine3.protobuf.Values.newStringValue;
 @SuppressWarnings("InstanceMethodNamingConvention")
 public class TypeNameShould {
 
+    private static final String STRING_VALUE_TYPE_NAME = "google.protobuf.StringValue";
+
     @Test(expected = NullPointerException.class)
     public void do_not_accept_null_value() {
         TypeName.of(Tests.<String>nullRef());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void do_not_accept_emtpy_string() {
+    public void do_not_accept_empty_string() {
         TypeName.of("");
     }
 
@@ -82,7 +83,7 @@ public class TypeNameShould {
         final Descriptors.Descriptor descriptor = StringValue.getDefaultInstance().getDescriptorForType();
 
         final TypeName typeName = TypeName.of(descriptor);
-        assertEquals(TypeName.of(StringValue.getDefaultInstance()), typeName);
+        assertEquals(STRING_VALUE_TYPE_NAME, typeName.value());
     }
 
     @Test
@@ -95,5 +96,12 @@ public class TypeNameShould {
 
         final TypeName typeName = TypeName.ofCommand(command);
         assertEquals(TypeName.of(message), typeName);
+    }
+
+    @Test
+    public void create_instance_by_class() {
+        final TypeName typeName = TypeName.of(StringValue.class);
+
+        assertEquals(STRING_VALUE_TYPE_NAME, typeName.value());
     }
 }
