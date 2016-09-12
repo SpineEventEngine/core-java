519b8551c5f7725460614b0da3f081263bd59e5d
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2016-09-12 21:03:41 +0300

    Add some Stand-based unit tests.

--- server/src/test/java/org/spine3/server/stand/StandShould.java
+++ server/src/test/java/org/spine3/server/stand/StandShould.java
@@ -21,6 +21,22 @@
  */
 package org.spine3.server.stand;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.protobuf.Descriptors;
+import org.junit.Assert;
+import org.junit.Test;
+import org.spine3.protobuf.TypeUrl;
+import org.spine3.server.BoundedContext;
+import org.spine3.server.projection.Projection;
+import org.spine3.server.projection.ProjectionRepository;
+import org.spine3.server.storage.StandStorage;
+import org.spine3.test.projection.Project;
+import org.spine3.test.projection.ProjectId;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.spine3.testdata.TestBoundedContextFactory.newBoundedContext;
+
 /**
  * @author Alex Tymchenko
  */
@@ -35,10 +51,79 @@ public class StandShould {
      * - avoid duplicates while registering repositories
      */
 
+    @Test
+    public void initialize_with_empty_builder() {
+        final Stand.Builder builder = Stand.newBuilder();
+        final Stand stand = builder.build();
+
+        Assert.assertNotNull(stand);
+        Assert.assertTrue("Available types must be empty after initialization.", stand.getAvailableTypes()
+                                                                                      .isEmpty());
+    }
+
+    @Test
+    // TODO[alex.tymchenko]: either add more meaningful checks or remove it.
+    public void initialize_with_storage_provided_through_builder() {
+        final StandStorage standStorageMock = spy(mock(StandStorage.class));
+        final Stand stand = Stand.newBuilder()
+                                 .setStorage(standStorageMock)
+                                 .build();
+        Assert.assertNotNull(stand);
+    }
+
+    @Test
+    public void register_projection_repositories() {
+        final Stand stand = Stand.newBuilder()
+                                 .build();
+        final BoundedContext boundedContext = newBoundedContext(stand);
+
+        Assert.assertTrue(stand.getAvailableTypes()
+                               .isEmpty());
+
+        final StandTestProjectionRepository standTestProjectionRepo = new StandTestProjectionRepository(boundedContext);
+        stand.registerTypeSupplier(standTestProjectionRepo);
+        checkHasExactlyOne(stand.getAvailableTypes(), Project.getDescriptor());
+
+        final StandTestProjectionRepository anotherTestProjectionRepo = new StandTestProjectionRepository(boundedContext);
+        stand.registerTypeSupplier(anotherTestProjectionRepo);
+        checkHasExactlyOne(stand.getAvailableTypes(), Project.getDescriptor());
+    }
+
+    private static void checkHasExactlyOne(ImmutableSet<TypeUrl> availableTypes, Descriptors.Descriptor expectedType) {
+        Assert.assertEquals(1, availableTypes.size());
+
+        final TypeUrl actualTypeUrl = availableTypes.iterator()
+                                                    .next();
+        final TypeUrl expectedTypeUrl = TypeUrl.of(expectedType);
+        Assert.assertEquals("Type was registered incorrectly", expectedTypeUrl, actualTypeUrl);
+    }
+
 
     // **** Negative scenarios ****
 
     /**
      * - fail to initialize with improper build arguments.
      */
+
+
+    // ***** Inner classes used for tests. *****
+
+    private static class StandTestProjection extends Projection<ProjectId, Project> {
+        /**
+         * Creates a new instance.
+         *
+         * @param id the ID for the new instance
+         * @throws IllegalArgumentException if the ID is not of one of the supported types
+         */
+        public StandTestProjection(ProjectId id) {
+            super(id);
+        }
+    }
+
+
+    private static class StandTestProjectionRepository extends ProjectionRepository<ProjectId, StandTestProjection, Project> {
+        protected StandTestProjectionRepository(BoundedContext boundedContext) {
+            super(boundedContext);
+        }
+    }
 }
