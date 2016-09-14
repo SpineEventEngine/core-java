ecfede812a9657acad0ac873636545f50f94ce28
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-09-14 23:29:36 +0300

    Remove redundant imports.

--- server/src/test/java/org/spine3/server/stand/StandShould.java
+++ server/src/test/java/org/spine3/server/stand/StandShould.java
@@ -41,15 +41,12 @@ import org.spine3.protobuf.AnyPacker;
 import org.spine3.protobuf.TypeUrl;
 import org.spine3.server.BoundedContext;
 import org.spine3.server.Given;
-import org.spine3.server.projection.Projection;
-import org.spine3.server.projection.ProjectionRepository;
-import org.spine3.server.storage.EntityStorageRecord;
 import org.spine3.server.stand.Given.StandTestProjectionRepository;
+import org.spine3.server.storage.EntityStorageRecord;
 import org.spine3.server.storage.StandStorage;
 import org.spine3.test.clientservice.customer.Customer;
 import org.spine3.test.clientservice.customer.CustomerId;
 import org.spine3.test.projection.Project;
-import org.spine3.test.projection.ProjectId;
 
 import java.util.List;
 import java.util.Objects;
