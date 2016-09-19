491ced213a89d814809f5ab523fdcd434ef22e8d
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-09-19 10:51:28 +0300

    Merge.

--- server/src/main/java/org/spine3/server/stand/Stand.java
+++ server/src/main/java/org/spine3/server/stand/Stand.java
@@ -62,6 +62,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
