521145d0e95dd46a94e2a35d93ba37f8e7d655cc
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-09-22 18:38:35 +0300

    Add creation tests for subscription service.

--- server/src/test/java/org/spine3/server/SubscriptionServiceShould.java
+++ server/src/test/java/org/spine3/server/SubscriptionServiceShould.java
@@ -20,10 +20,81 @@
 
 package org.spine3.server;
 
+import org.junit.Test;
+import org.spine3.server.stand.Stand;
+import org.spine3.server.storage.memory.InMemoryStandStorage;
+import org.spine3.server.storage.memory.InMemoryStorageFactory;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * @author Dmytro Dashenkov
  */
 public class SubscriptionServiceShould {
 
+    /*
+     * Creation tests
+     * --------------
+     */
 
+    @Test
+    public void initialize_properly_with_one_bounded_context() {
+        final BoundedContext singleBoundedContext = newBoundedContext("Single");
+
+        final SubscriptionService subscriptionService = SubscriptionService.newBuilder()
+                                                                           .addBoundedContext(singleBoundedContext)
+                                                                           .build();
+
+        assertNotNull(subscriptionService);
+    }
+
+    @Test
+    public void initialize_properly_with_several_bounded_contexts() {
+        final BoundedContext firstBoundedContext = newBoundedContext("First");
+        final BoundedContext secondBoundedContext = newBoundedContext("Second");
+        final BoundedContext thirdBoundedContext = newBoundedContext("Third");
+
+
+        final SubscriptionService subscriptionService = SubscriptionService.newBuilder()
+                                                                           .addBoundedContext(firstBoundedContext)
+                                                                           .addBoundedContext(secondBoundedContext)
+                                                                           .addBoundedContext(thirdBoundedContext)
+                                                                           .build();
+
+        assertNotNull(subscriptionService);
+    }
+
+    @Test
+    public void be_able_to_remove_bounded_context_from_builder() {
+        final BoundedContext firstBoundedContext = newBoundedContext("Removed");
+        final BoundedContext secondBoundedContext = newBoundedContext("Also removed");
+        final BoundedContext thirdBoundedContext = newBoundedContext("The one to stay");
+
+
+        final SubscriptionService subscriptionService = SubscriptionService.newBuilder()
+                                                                           .addBoundedContext(firstBoundedContext)
+                                                                           .addBoundedContext(secondBoundedContext)
+                                                                           .addBoundedContext(thirdBoundedContext)
+                                                                           .removeBoundedContext(secondBoundedContext)
+                                                                           .removeBoundedContext(firstBoundedContext)
+                                                                           .build();
+
+        assertNotNull(subscriptionService);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void fail_to_initialize_from_empty_builder() {
+        SubscriptionService.newBuilder().build();
+    }
+
+    private static BoundedContext newBoundedContext(String name) {
+        final Stand stand = Stand.newBuilder().setStorage(InMemoryStandStorage.newBuilder().build()).build();
+
+        return BoundedContext.newBuilder()
+                             .setStand(stand)
+                             .setName(name)
+                             .setStorageFactory(InMemoryStorageFactory.getInstance())
+                             .build();
+
+    }
 }
