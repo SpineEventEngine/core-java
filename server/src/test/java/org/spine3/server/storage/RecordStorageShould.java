69ea7726cef8d0b8378b2ea57243aad5df29a447
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-10-10 20:22:59 +0300

    Cover InMemoryRecordStorage#readAll(FieldMask) empty storage case with a test.

---
+++ server/src/test/java/org/spine3/server/storage/RecordStorageShould.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.server.storage;
+
+import com.google.protobuf.FieldMask;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.spine3.test.Verify.assertEmpty;
+
+/**
+ * @author Dmytro Dashenkov
+ */
+public abstract class RecordStorageShould {
+
+    @Test
+    public void retrieve_empty_map_if_storage_is_empty() {
+        final RecordStorage<String> storage = createStorage();
+
+        final Map empty = storage.readAll(FieldMask.getDefaultInstance());
+        assertNotNull(empty);
+        assertEmpty(empty);
+    }
+
+    protected abstract <T> RecordStorage<T> createStorage();
+}
