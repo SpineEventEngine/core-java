ba08bbba1d14b12dca7f83d4bdb90cfe06929629
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-10-12 11:45:37 +0300

    Add read-all tests for record storage.

--- server/src/test/java/org/spine3/server/storage/RecordStorageShould.java
+++ server/src/test/java/org/spine3/server/storage/RecordStorageShould.java
@@ -20,13 +20,24 @@
 
 package org.spine3.server.storage;
 
+import com.google.common.base.Supplier;
 import com.google.protobuf.FieldMask;
 import org.junit.Test;
+import org.spine3.protobuf.AnyPacker;
+import org.spine3.protobuf.Timestamps;
+import org.spine3.test.projection.Project;
+import org.spine3.test.projection.ProjectId;
+import org.spine3.test.projection.Task;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import static org.junit.Assert.assertNotNull;
 import static org.spine3.test.Verify.assertEmpty;
+import static org.spine3.test.Verify.assertSize;
 
 /**
  * @author Dmytro Dashenkov
@@ -44,6 +55,48 @@ public abstract class RecordStorageShould {
         final Map empty = storage.readAll(nonEmptyFieldMask);
         assertNotNull(empty);
         assertEmpty(empty);
+    }
+
+    @Test
+    public void retrieve_all_records() {
+        final RecordStorage<String> storage = createStorage();
+        final Collection<String> ids = fill(storage, 10, new Supplier<String>() {
+            @Override
+            public String get() {
+                return UUID.randomUUID()
+                           .toString();
+            }
+        });
+
+        final Map<String, EntityStorageRecord> allRecords = storage.readAll();
+        assertSize(ids.size(), allRecords);
+
+    }
+
+    protected static <T> List<T> fill(RecordStorage<T> storage, int count, Supplier<T> idSupplier) {
+        final List<T> ids = new LinkedList<>();
+
+        for (int i = 0; i < count; i++) {
+            final T genericId = idSupplier.get();
+            final ProjectId id = ProjectId.newBuilder()
+                                          .setId(genericId.toString())
+                                          .build();
+            final Project project = Project.newBuilder()
+                                           .setId(id)
+                                           .setStatus(Project.Status.CREATED)
+                                           .setName(String.format("test-project-%s", i))
+                                           .addTask(Task.getDefaultInstance())
+                                           .build();
+            final EntityStorageRecord record = EntityStorageRecord.newBuilder()
+                                                                  .setState(AnyPacker.pack(project))
+                                                                  .setWhenModified(Timestamps.getCurrentTime())
+                                                                  .setVersion(1)
+                                                                  .build();
+            storage.write(genericId, record);
+            ids.add(genericId);
+        }
+
+        return ids;
     }
 
     protected abstract <T> RecordStorage<T> createStorage();
