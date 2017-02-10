0e8eb6644ac438efbd7e289fa93d66e96b2d2720
a.aleksandrov <aleksandrov.aleksander.a@gmail.com>
2017-02-10 14:35:40 +0200

    Change Javadoc in `Subscribe`

--- server/src/main/java/org/spine3/server/event/Subscribe.java
+++ server/src/main/java/org/spine3/server/event/Subscribe.java
@@ -20,9 +20,6 @@
 
 package org.spine3.server.event;
 
-import com.google.protobuf.Message;
-import org.spine3.base.EventContext;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -36,8 +33,8 @@ import java.lang.annotation.Target;
  *     <li>is annotated with {@link Subscribe};
  *     <li>is {@code public};
  *     <li>returns {@code void};
- *     <li>accepts an event derived from {@link Message} as the first parameter;
- *     <li>(optional) accepts an {@link EventContext} as the second parameter.
+ *     <li>accepts an event derived from {@link com.google.protobuf.Message} as the first parameter;
+ *     <li>(optional) accepts an {@link org.spine3.base.EventContext} as the second parameter.
  * </ul>
  *
  * If the annotation is applied to a method which doesn't satisfy any of these requirements, this method is not
