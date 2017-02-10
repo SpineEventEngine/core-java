2d7cdec78a2a7892d48e45cd9700d9d8a62e55d2
a.aleksandrov <aleksandrov.aleksander.a@gmail.com>
2017-02-10 18:41:09 +0200

    Fix Javadoc for unpack(Any).

--- client/src/main/java/org/spine3/protobuf/AnyPacker.java
+++ client/src/main/java/org/spine3/protobuf/AnyPacker.java
@@ -71,13 +71,10 @@ public class AnyPacker {
      * Unwraps {@code Any} value into an instance of type specified by value
      * returned by {@link Any#getTypeUrl()}.
      *
-     * <p>If there is no Java class for the type, {@link org.spine3.protobuf.error.UnknownTypeException}
-     * will be thrown.
      *
      * @param any instance of {@link Any} that should be unwrapped
      * @param <T> the type enclosed into {@code Any}
      * @return unwrapped message instance
-     * @throws org.spine3.protobuf.error.UnknownTypeException if there is no Java class in the classpath for the enclosed type
      */
     public static <T extends Message> T unpack(Any any) {
         checkNotNull(any);
@@ -88,6 +85,9 @@ public class AnyPacker {
 
     /**
      * Unwraps {@code Any} value into an instance of the passed class.
+     *
+     * <p>If there is no Java class for the type, {@link org.spine3.protobuf.error.UnexpectedTypeException}
+     * will be thrown.
      *
      * @param any   instance of {@link Any} that should be unwrapped
      * @param clazz the class implementing the type of the enclosed object
