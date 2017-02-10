e5deb7c28c9da0be91fc64d29df545e8250ac05c
a.aleksandrov <aleksandrov.aleksander.a@gmail.com>
2017-02-10 15:01:20 +0200

    Change Javadoc in `StandStorage`

--- server/src/main/java/org/spine3/server/stand/StandStorage.java
+++ server/src/main/java/org/spine3/server/stand/StandStorage.java
@@ -23,12 +23,11 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.protobuf.FieldMask;
 import org.spine3.SPI;
 import org.spine3.protobuf.TypeUrl;
-import org.spine3.server.aggregate.Aggregate;
 import org.spine3.server.storage.EntityStorageRecord;
 import org.spine3.server.storage.RecordStorage;
 
 /**
- * Serves as a storage for the latest {@link Aggregate} states.
+ * Serves as a storage for the latest {@link org.spine3.server.aggregate.Aggregate} states.
  *
  * <p>Used by an instance of {@link Stand} to optimize the {@code Aggregate} state fetch performance.
  *
