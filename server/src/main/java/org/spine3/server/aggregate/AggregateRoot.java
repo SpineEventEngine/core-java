95ea55e63ec1f041dcfb661f2f2c827c34e92b59
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2017-02-28 22:00:56 +0200

    Fix Javadoc

--- server/src/main/java/org/spine3/server/aggregate/AggregateRoot.java
+++ server/src/main/java/org/spine3/server/aggregate/AggregateRoot.java
@@ -65,26 +65,27 @@ public class AggregateRoot<I> {
     }
 
     /**
-     * Creates a new {@code AggregateRoot} entity and sets it to the default state.
+     * Creates a new {@code AggregateRoot}.
      *
-     * @param id        the ID of the {@code AggregatePart} is managed by {@code AggregateRoot}
-     * @param ctx       the {@code BoundedContext} to use
-     * @param rootClass the class of the {@code AggregateRoot}
-     * @param <I>       the type of entity IDs
-     * @return an {@code AggregateRoot} instance
+     * @param aggregateId    the ID of the aggregate
+     * @param boundedContext the {@code BoundedContext} to use
+     * @param rootClass      the class of the {@code AggregateRoot}
+     * @param <I>            the type of entity IDs
+     * @return new instance
      */
-    static <I, R extends AggregateRoot<I>> R create(I id,
-                                                    BoundedContext ctx,
+    static <I, R extends AggregateRoot<I>> R create(I aggregateId,
+                                                    BoundedContext boundedContext,
                                                     Class<R> rootClass) {
-        checkNotNull(id);
-        checkNotNull(ctx);
+        checkNotNull(aggregateId);
+        checkNotNull(boundedContext);
         checkNotNull(rootClass);
 
         try {
             final Constructor<R> rootConstructor =
-                    rootClass.getDeclaredConstructor(ctx.getClass(), id.getClass());
+                    rootClass.getDeclaredConstructor(boundedContext.getClass(),
+                                                     aggregateId.getClass());
             rootConstructor.setAccessible(true);
-            final R root = rootConstructor.newInstance(ctx, id);
+            final R root = rootConstructor.newInstance(boundedContext, aggregateId);
             return root;
         } catch (NoSuchMethodException | InvocationTargetException |
                 InstantiationException | IllegalAccessException e) {
@@ -116,8 +117,8 @@ public class AggregateRoot<I> {
      *                               or the ID type of the part state does not match
      *                               the ID type of this {@code AggregateRoot}
      */
-    protected <S extends Message, A extends AggregatePart<I, S, ?, ?>> S
-    getPartState(Class<S> partStateClass) {
+    protected <S extends Message, A extends AggregatePart<I, S, ?, ?>>
+    S getPartState(Class<S> partStateClass) {
         final AggregatePartRepository<I, A, ?> repo = getRepository(partStateClass);
         final AggregatePart<I, S, ?, ?> aggregatePart = repo.loadOrCreate(getId());
         final S partState = aggregatePart.getState();
