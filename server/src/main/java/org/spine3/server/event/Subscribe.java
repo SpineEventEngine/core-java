de375b439286f8c997382502ed1af9fee74cd6f7
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-03-02 13:47:11 +0200

    Update the `Subscribe` annotation documentation according to the design intention.

--- server/src/main/java/org/spine3/server/event/Subscribe.java
+++ server/src/main/java/org/spine3/server/event/Subscribe.java
@@ -26,7 +26,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a method as an event subscriber.
+ * Marks a method as a subscriber for the command output.
+ *
+ * <p>Use it to subscribe to either events or business failures
+ *
+ * <h2>Subscribing to Events</h2>
  *
  * <p>An event subscriber method:
  * <ul>
@@ -39,9 +43,33 @@ import java.lang.annotation.Target;
  *          as the second parameter.
  * </ul>
  *
- * If the annotation is applied to a method which doesn't satisfy any of these requirements,
- * this method is not considered as an event subscriber and is not registered for event delivery
- * from {@link EventBus}.
+ * <h2>Subscribing to Failures</h2>
+ *
+ * <p>A failure subscriber method:
+ * <ul>
+ *     <li>is annotated with {@link Subscribe};
+ *     <li>is {@code public};
+ *     <li>returns {@code void};
+ *     <li>accepts a failure derived from {@link com.google.protobuf.Message Message}
+ *          as the first parameter;
+ *     <li>accepts a command derived from {@link com.google.protobuf.Message Message}
+ *          as the second parameter;
+ *     <li>(optional) accepts an {@link org.spine3.base.CommandContext CommandContext}
+ *          as the third parameter.
+ * </ul>
+ *
+ * <p>The type of the command argument specified acts as a filter. I.e.
+ * the subscriber receives the failure if:
+ * <ul>
+ *     <li>the failure type matches the first argument type;
+ *     <li>the command, which processing caused the failure, has the same type as the
+ *          second argument.
+ * </ul>
+ *
+ *
+ * <p>If the annotation is applied to a method which doesn't satisfy any of these requirements,
+ * this method is not considered as a subscriber and is not registered for the command output
+ * delivery from the relevant {@linkplain org.spine3.server.bus.Bus bus}.
  *
  * @author Alexander Yevsyukov
  */
