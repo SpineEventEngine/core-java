ce41a5c5556475a242ab3ac108a9f63c8cf9a077
illiashepilov <illia.shepilov@teamdev.com>
2017-03-02 12:30:37 +0200

    Create `ConversionError`.

---
+++ client/src/main/java/org/spine3/validate/ConversionError.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.validate;
+
+import java.text.ParseException;
+
+/**
+ * @author Illia Shepilov
+ */
+@SuppressWarnings("ExceptionClassNameDoesntEndWithException")
+// It is OK because it is not {@code Exception} in usual meaning.
+public class ConversionError extends ParseException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a ParseException with the specified detail message and
+     * offset.
+     * A detail message is a String that describes this particular exception.
+     *
+     * @param s           the detail message
+     * @param errorOffset the position where the error is found while parsing.
+     */
+    public ConversionError(String s, int errorOffset) {
+        super(s, errorOffset);
+    }
+}
