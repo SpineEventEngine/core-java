b3d182ca8abd03d980145fec5ea19e3540bfc428
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-03-02 15:55:54 +0200

    Extend `ConstraintViolations` with an ability to handle nested violations and just the `Iterable` of the violations.

--- client/src/main/java/org/spine3/validate/ConstraintViolations.java
+++ client/src/main/java/org/spine3/validate/ConstraintViolations.java
@@ -44,25 +44,89 @@ public class ConstraintViolations {
      */
     public static String toText(ConstraintViolation violation) {
         checkNotNull(violation);
+
         final String format = violation.getMsgFormat();
         final List<String> params = violation.getParamList();
-        final String result = String.format(format, params.toArray());
-        return result;
+        final String parentViolationFormatted = String.format(format, params.toArray());
+
+        final StringBuilder resultBuilder = new StringBuilder(parentViolationFormatted);
+        if (violation.getViolationCount() > 0) {
+            resultBuilder.append(toText(violation.getViolationList()));
+        }
+        return resultBuilder.toString();
     }
 
     /**
-     * Returns a formatted string using the specified format string and parameters from the violation.
+     * Returns a formatted string using the format string and parameters from each of
+     * the violations passed.
+     *
+     * @param violations violations which contain the format string and
+     *                   arguments referenced by the format specifiers in each of them
+     * @return a formatted string
+     * @see #toText(ConstraintViolation)
+     */
+    @SuppressWarnings("HardcodedLineSeparator")     // required for better formatting.
+    public static String toText(Iterable<ConstraintViolation> violations) {
+        checkNotNull(violations);
+
+        final StringBuilder resultBuilder = new StringBuilder("Violation list:");
+
+        for (ConstraintViolation childViolation : violations) {
+            final String childViolationFormatted = toText(childViolation);
+            resultBuilder.append("\n  ")
+                         .append(childViolationFormatted);
+        }
+        return resultBuilder.toString();
+    }
+
+    /**
+     * Returns a formatted string using the specified format string and parameters
+     * from the violation.
      *
      * @param format    a format string
-     * @param violation violation which contains arguments referenced by the format specifiers in the format string
+     * @param violation violation which contains arguments referenced by the format
+     *                  specifiers in the format string
      * @return a formatted string
      * @see String#format(String, Object...)
      */
+    @SuppressWarnings("HardcodedLineSeparator")     // required for better formatting.
     public static String toText(String format, ConstraintViolation violation) {
         checkNotNull(format);
         checkNotNull(violation);
+
         final List<String> params = violation.getParamList();
-        final String result = String.format(format, params.toArray());
-        return result;
+        final String parentViolationFormatted = String.format(format, params.toArray());
+
+        final StringBuilder resultBuilder = new StringBuilder(parentViolationFormatted);
+        if (violation.getViolationCount() > 0) {
+            resultBuilder.append(toText(format, violation.getViolationList()));
+        }
+
+        return resultBuilder.toString();
+    }
+
+    /**
+     * Returns a formatted string using the specified format string and parameters from
+     * each of the violations passed.
+     *
+     * @param format     a format string
+     * @param violations violations which contain the arguments referenced by the format
+     *                   specifiers in the format string
+     * @return a formatted string
+     * @see String#format(String, Object...)
+     */
+    @SuppressWarnings("HardcodedLineSeparator")     // required for better formatting.
+    public static String toText(String format, Iterable<ConstraintViolation> violations) {
+        checkNotNull(format);
+        checkNotNull(violations);
+
+        final StringBuilder resultBuilder = new StringBuilder("Violations:");
+
+        for (ConstraintViolation childViolation : violations) {
+            final String childViolationFormatted = toText(format, childViolation);
+            resultBuilder.append("\n  ")
+                         .append(childViolationFormatted);
+        }
+        return resultBuilder.toString();
     }
 }
