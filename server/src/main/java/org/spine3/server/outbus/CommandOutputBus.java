59e6d9dacc8c3b2cd726f17c7797a4e6897c7677
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-03-02 14:35:44 +0200

    Create an abstract base for the `EventBus` and `FailureBus`.

---
+++ server/src/main/java/org/spine3/server/outbus/CommandOutputBus.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.spine3.server.outbus;
+
+import com.google.protobuf.Message;
+import org.spine3.base.MessageClass;
+import org.spine3.base.MessageEnvelope;
+import org.spine3.server.bus.Bus;
+import org.spine3.server.bus.MessageDispatcher;
+
+/**
+ * A base bus responsible for delivering the {@link org.spine3.base.Command command} output.
+ *
+ * <p>The typical output artifacts of the command processing are:
+ *
+ * <ul>
+ *     <li>{@linkplain org.spine3.base.Event events} — in case the command is handled successfully;
+ *     <li>{@linkplain org.spine3.base.Failure business failures} — if the command contradicts
+ *          the business rules.
+ * </ul>
+ *
+ * <p>The instances of {@code CommandOutputBus} are responsible for a delivery of such output
+ * artifacts to the corresponding destinations.
+ *
+ * @author Alex Tymchenko
+ */
+public abstract class CommandOutputBus< M extends Message,
+                                        E extends MessageEnvelope<M>,
+                                        C extends MessageClass,
+                                        D extends MessageDispatcher<C,E>> extends Bus<M, E, C, D> {
+
+}
