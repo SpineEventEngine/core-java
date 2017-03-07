f82582e40cfe9af33497d368ebef98e15d168b26
illiashepilov <illia.shepilov@teamdev.com>
2017-03-07 10:12:48 +0200

    Create the `RegistryKey` hierarchy.

---
+++ client/src/main/java/org/spine3/base/PluralKey.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.base;
+
+import com.google.common.base.Objects;
+
+/**
+ * @author Illia Shepilov
+ */
+public class PluralKey<A, B> implements RegistryKey {
+
+    private final Class<A> typeClass;
+    private final Class<B> genericClass;
+
+    public PluralKey(Class<A> typeClass, Class<B> genericClass) {
+        this.typeClass = typeClass;
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PluralKey<?, ?> pluralKey = (PluralKey<?, ?>) o;
+        return Objects.equal(typeClass, pluralKey.typeClass) &&
+               Objects.equal(genericClass, pluralKey.genericClass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(typeClass, genericClass);
+    }
+}
