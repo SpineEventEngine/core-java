4192217e2f1976a5cc032fb90da9f2114ab17a94
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-03-13 15:28:00 +0200

    Add code structure for implementing the storage fields extraction.

---
+++ server/src/main/java/org/spine3/server/entity/storagefields/StorageFieldsGenerator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.server.entity.storagefields;
+
+import com.google.common.base.Function;
+import org.spine3.server.entity.Entity;
+import org.spine3.server.entity.StorageFields;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author Dmytro Dashenkov.
+ */
+class StorageFieldsGenerator implements Function<Entity<?, ?>, StorageFields> {
+
+    @Override
+    public StorageFields apply(@Nullable Entity<?, ?> input) {
+        return StorageFields.getDefaultInstance();
+    }
+}
