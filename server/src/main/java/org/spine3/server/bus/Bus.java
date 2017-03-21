3ef97f65f5644dc3852d65cc9ee6a2992c29be18
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-03-21 17:37:13 +0200

    Remove an unused import statement.

--- server/src/main/java/org/spine3/server/bus/Bus.java
+++ server/src/main/java/org/spine3/server/bus/Bus.java
@@ -24,7 +24,6 @@ import com.google.protobuf.Message;
 import io.grpc.stub.StreamObserver;
 import org.spine3.base.Response;
 import org.spine3.envelope.MessageEnvelope;
-import org.spine3.server.command.CommandDispatcher;
 import org.spine3.type.MessageClass;
 
 import javax.annotation.Nullable;
