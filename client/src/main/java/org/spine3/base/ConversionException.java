58fd3763f055fcc26de4d1b4614530a4b4aaef5c
illiashepilov <illia.shepilov@teamdev.com>
2017-03-22 19:43:37 +0200

    Add constructor argument to the `ConversionException` class.

--- client/src/main/java/org/spine3/base/ConversionException.java
+++ client/src/main/java/org/spine3/base/ConversionException.java
@@ -29,7 +29,7 @@ public class ConversionException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    public ConversionException(String message) {
-        super(message);
+    public ConversionException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
