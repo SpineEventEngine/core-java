bea0cbfd98d3204c393d4266ffe8b515f2422e6e
illiashepilov <illia.shepilov@teamdev.com>
2017-03-28 16:17:06 +0300

    Fix the exception message.

--- client/src/main/java/org/spine3/base/MapStringifier.java
+++ client/src/main/java/org/spine3/base/MapStringifier.java
@@ -74,7 +74,7 @@ class MapStringifier<K, V> extends Stringifier<Map<K, V>> {
 
     private static final char DEFAULT_ELEMENT_DELIMITER = ',';
     private static final char KEY_VALUE_DELIMITER = ':';
-    public static final String THE_KEY_VALUE_ENTRY_CANNOT_BE_NULL = "The key value entry cannot be null.";
+    public static final String KEY_VALUE_ARE_NULL_EX_MSG = "The key and value cannot be null.";
 
     /**
      * The delimiter for the passed elements in the {@code String} representation,
@@ -176,7 +176,7 @@ class MapStringifier<K, V> extends Stringifier<Map<K, V>> {
             @Override
             public QuotedMapItem<K, V> transformEntry(@Nullable K key, @Nullable V value) {
                 if (key == null || value == null) {
-                    throw newIllegalArgumentException(THE_KEY_VALUE_ENTRY_CANNOT_BE_NULL);
+                    throw newIllegalArgumentException(KEY_VALUE_ARE_NULL_EX_MSG);
                 }
                 return of(key, value);
             }
