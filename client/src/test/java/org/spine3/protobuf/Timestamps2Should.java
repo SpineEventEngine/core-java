b613a465b83490c8aab8894e243adb1158ab0cc0
a.aleksandrov <aleksandrov.aleksander.a@gmail.com>
2017-03-28 18:16:38 +0300

    Add fixes for client module.

--- client/src/test/java/org/spine3/protobuf/Timestamps2Should.java
+++ client/src/test/java/org/spine3/protobuf/Timestamps2Should.java
@@ -203,7 +203,7 @@ public class Timestamps2Should {
     }
 
     @Test
-    public void compare_two_timestamps_using_comparator_return_negative_int_if_first_less_than_second_one() {
+    public void compare_timestamps_return_negative_int_if_first_less_than_second_one() {
         final Timestamp time1 = getCurrentTime();
         final Timestamp time2 = add(time1, TEN_SECONDS);
 
@@ -233,7 +233,7 @@ public class Timestamps2Should {
     }
 
     @Test
-    public void compare_two_timestamps_using_comparator_return_positive_int_if_first_greater_than_second_one() {
+    public void compare_timestamps_return_positive_int_if_first_greater_than_second_one() {
         final Timestamp currentTime = getCurrentTime();
         final Timestamp timeAfterCurrent = add(currentTime, TEN_SECONDS);
 
