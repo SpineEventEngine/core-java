7dc30b60d694ada8215f8fa499ff7b3286f420f3
a.aleksandrov <aleksandrov.aleksander.a@gmail.com>
2017-04-21 17:05:14 +0300

    Fixed Codacy issue for unnecessary if..then..else statement.

--- client/src/main/java/org/spine3/time/LocalDates.java
+++ client/src/main/java/org/spine3/time/LocalDates.java
@@ -210,10 +210,7 @@ public final class LocalDates {
         }
     }
     private static boolean yearIsLeap(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            return true;
-        }
-        return false;
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
     public static int daysInMonth(int month, int year) {
