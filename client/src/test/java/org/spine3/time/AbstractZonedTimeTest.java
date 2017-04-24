7724935ec4eb90341fd694dc10ac0b1bb3ef86b2
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-04-24 15:09:35 +0300

    Re-enable the failing tests and turn on the std output streams redirection to the Gradle log.

--- client/src/test/java/org/spine3/time/AbstractZonedTimeTest.java
+++ client/src/test/java/org/spine3/time/AbstractZonedTimeTest.java
@@ -21,7 +21,6 @@
 package org.spine3.time;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -64,7 +63,7 @@ public abstract class AbstractZonedTimeTest {
         assertConversionAt(ZoneOffsets.UTC);
     }
 
-    @Ignore //TODO:2017-04-21:alexander.yevsyukov: Enable back when able to get failing test logs from Travis
+//    @Ignore //TODO:2017-04-21:alexander.yevsyukov: Enable back when able to get failing test logs from Travis
     @Test
     public void convert_values_at_current_time_zone() throws ParseException {
         // Get current zone offset and strip ID value because it's not stored into date/time.
