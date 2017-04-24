eb943a39e480af387b9e155d92a488b0de6dba7a
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2017-04-24 23:38:51 +0300

    Test memoizing `StreamObserver`.

--- client/src/test/java/org/spine3/io/StreamObserversShould.java
+++ client/src/test/java/org/spine3/io/StreamObserversShould.java
@@ -19,11 +19,24 @@
  */
 package org.spine3.io;
 
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Range;
 import io.grpc.stub.StreamObserver;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
+import org.spine3.io.StreamObservers.MemoizingObserver;
 
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -61,5 +74,60 @@ public class StreamObserversShould {
         verify(delegate, times(1)).onError(ArgumentMatchers.eq(errorToForward));
         verify(delegate, never()).onNext(ArgumentMatchers.any());
         verify(delegate, never()).onCompleted();
+    }
+
+    @Test
+    public void create_proper_memoizing_observer() {
+        final MemoizingObserver<Object> observer = StreamObservers.memoizingObserver();
+
+        checkFirstResponse(observer);
+        checkOnNext(observer);
+        checkOnError(observer);
+        checkIsCompleted(observer);
+    }
+
+    private static void checkFirstResponse(MemoizingObserver<Object> observer) {
+        try {
+            observer.firstResponse();
+            fail("ISE expected for the MemoizingObserver.firstResponse() if it is empty, " +
+                         "but got nothing.");
+        } catch (IllegalStateException e) {
+            // as expected
+        }
+    }
+
+    private static void checkOnNext(MemoizingObserver<Object> observer) {
+        assertTrue(observer.responses().isEmpty());
+
+        final Object firstResponse = new Object();
+        observer.onNext(firstResponse);
+        assertEquals(firstResponse, observer.firstResponse());
+
+        final ContiguousSet<Integer> sorted = ContiguousSet.create(Range.closed(1, 20),
+                                                                   DiscreteDomain.integers());
+        final List<Integer> moreResponses = newArrayList(newHashSet(sorted));
+
+        for (Integer element : moreResponses) {
+            observer.onNext(element);
+        }
+        final List<Object> actualResponses = observer.responses();
+
+        assertEquals(firstResponse, actualResponses.get(0));
+        assertEquals(moreResponses.size() + 1, // as there was the first response
+                     actualResponses.size());
+        assertEquals(moreResponses, actualResponses.subList(1, actualResponses.size()));
+    }
+
+    private static void checkOnError(MemoizingObserver<Object> observer) {
+        assertNull(observer.getError());
+        final RuntimeException exception = new RuntimeException("Sample error");
+        observer.onError(exception);
+        assertEquals(exception, observer.getError());
+    }
+
+    private static void checkIsCompleted(MemoizingObserver<Object> observer) {
+        assertFalse(observer.isCompleted());
+        observer.onCompleted();
+        assertTrue(observer.isCompleted());
     }
 }
