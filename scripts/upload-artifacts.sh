19fe558ae73105b2e8b42d44e37786481b263a36
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-05-12 14:49:52 +0300

    Move Travis artifacts location to `gcs.properties`

--- scripts/upload-artifacts.sh
+++ scripts/upload-artifacts.sh
@@ -8,11 +8,17 @@ gem instal dpl
 mkdir reports
 zip -r reports/test-reports.zip **/build/reports
 zip -r reports/jacoco-reports.zip **/build/jacoco
+
+# Returns the value for the specified key.
+function getProp() {
+    grep "${1}" gcs.properties | cut -d'=' -f2
+}
+
 # Upload the prepared reports to GCS.
 dpl --provider=gcs \
     --access-key-id=GOOGX66ER6DXLZH7IKQF \
     --secret-access-key=${GCS_SECRET} \
-    --bucket=spine-dev.appspot.com \
-    --upload-dir=core-java/builds/${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER} \
+    --bucket="$(getProp 'artifacts.bucket')" \
+    --upload-dir="$(getProp 'artifacts.folder')"/${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER} \
     --local-dir=reports \
     --skip_cleanup=true
