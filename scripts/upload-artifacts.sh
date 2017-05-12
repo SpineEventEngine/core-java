eef7cea58e733061ef579746ec9dd8ff48787864
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-05-12 13:16:55 +0300

    Add correct transfers for multiline command

--- scripts/upload-artifacts.sh
+++ scripts/upload-artifacts.sh
@@ -9,10 +9,10 @@ mkdir reports
 zip -r reports/test-reports.zip **/build/reports
 zip -r reports/jacoco-reports.zip **/build/jacoco
 # Upload the prepared reports to GCS.
-dpl --provider=gcs
-    --access-key-id=GOOGX66ER6DXLZH7IKQF
-    --secret-access-key=${GCS_SECRET}
-    --bucket=spine-dev.appspot.com
-    --upload-dir=core-java/builds/${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}
-    --local-dir=reports
+dpl --provider=gcs \
+    --access-key-idGOOGX66ER6DXLZH7IKQF \
+    --secret-access-key=${GCS_SECRET} \
+    --bucket=spine-dev.appspot.com \
+    --upload-dir=core-java/builds/${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER} \
+    --local-dir=reports \
     --skip_cleanup=true
