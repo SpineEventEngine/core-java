267e5e3200b7da6a530f6eaf5179c6c137fb287b
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-05-23 12:45:04 +0300

    Add 2 new test cases for the EventStore.iterator.

--- server/src/test/java/org/spine3/server/event/EventStoreShould.java
+++ server/src/test/java/org/spine3/server/event/EventStoreShould.java
@@ -31,6 +31,7 @@ import org.spine3.base.Commands;
 import org.spine3.base.Event;
 import org.spine3.test.TestEventFactory;
 import org.spine3.test.event.ProjectCreated;
+import org.spine3.test.event.TaskAdded;
 import org.spine3.testdata.Sample;
 import org.spine3.time.Durations2;
 import org.spine3.time.ZoneOffsets;
@@ -46,8 +47,10 @@ import static com.google.protobuf.util.Timestamps.add;
 import static com.google.protobuf.util.Timestamps.subtract;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.spine3.test.Verify.assertContainsAll;
 import static org.spine3.test.Verify.assertSize;
 import static org.spine3.time.Time.getCurrentTime;
+import static org.spine3.type.TypeName.of;
 
 /**
  * @author Dmytro Dashenkov
@@ -105,9 +108,78 @@ public abstract class EventStoreShould {
         assertEquals(eventInPresent, event);
     }
 
-    private static Event projectCreated(Timestamp timestamp) {
+    @Test
+    public void read_events_by_time_and_type() {
+        final Duration delta = Durations2.seconds(111);
+        final Timestamp present = getCurrentTime();
+        final Timestamp past = subtract(present, delta);
+        final Timestamp future = add(present, delta);
+
+        final Event eventInPast = taskAdded(past);
+        final Event eventInPresent = projectCreated(present);
+        final Event eventInFuture = taskAdded(future);
+
+        eventStore.append(eventInPast);
+        eventStore.append(eventInPresent);
+        eventStore.append(eventInFuture);
+
+        final EventFilter taskAddedType = EventFilter.newBuilder()
+                                                       .setEventType(of(TaskAdded.class).value())
+                                                       .build();
+        final EventStreamQuery query = EventStreamQuery.newBuilder()
+                                                       .setAfter(past)
+                                                       .addFilter(taskAddedType)
+                                                       .build();
+        final AtomicBoolean done = new AtomicBoolean(false);
+        final Collection<Event> resultEvents = Collections.synchronizedSet(new HashSet<Event>());
+        eventStore.read(query, new ResponseObserver(resultEvents, done));
+        if (!done.get()) {
+            fail("Please use the MoreExecutors.directExecutor in EventStore for tests.");
+        }
+
+        assertSize(1, resultEvents);
+        final Event event = resultEvents.iterator()
+                                        .next();
+        assertEquals(eventInFuture, event);
+    }
+
+    @Test
+    public void read_events_by_type() {
+        final Timestamp now = getCurrentTime();
+
+        final Event taskAdded1 = taskAdded(now);
+        final Event projectCreated = projectCreated(now);
+        final Event teasAdded2 = taskAdded(now);
+
+        eventStore.append(taskAdded1);
+        eventStore.append(projectCreated);
+        eventStore.append(teasAdded2);
+
+        final EventFilter taskAddedType = EventFilter.newBuilder()
+                                                     .setEventType(of(TaskAdded.class).value())
+                                                     .build();
+        final EventStreamQuery query = EventStreamQuery.newBuilder()
+                                                       .addFilter(taskAddedType)
+                                                       .build();
+        final AtomicBoolean done = new AtomicBoolean(false);
+        final Collection<Event> resultEvents = Collections.synchronizedSet(new HashSet<Event>());
+        eventStore.read(query, new ResponseObserver(resultEvents, done));
+        if (!done.get()) {
+            fail("Please use the MoreExecutors.directExecutor in EventStore for tests.");
+        }
+
+        assertSize(2, resultEvents);
+        assertContainsAll(resultEvents, taskAdded1, teasAdded2);
+    }
+
+    private static Event projectCreated(Timestamp when) {
         final ProjectCreated msg = Sample.messageOfType(ProjectCreated.class);
-        return eventFactory.createEvent(msg, null, timestamp);
+        return eventFactory.createEvent(msg, null, when);
+    }
+
+    private static Event taskAdded(Timestamp when) {
+        final TaskAdded msg = Sample.messageOfType(TaskAdded.class);
+        return eventFactory.createEvent(msg, null, when);
     }
 
     private static class ResponseObserver implements StreamObserver<Event> {
