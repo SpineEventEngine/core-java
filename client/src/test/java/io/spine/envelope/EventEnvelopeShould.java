dfcdf7987c2fc280bc16c90f17ec7848291ea3c3
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-06-19 12:37:02 +0300

    Add message envelope tests.

---
+++ client/src/test/java/io/spine/envelope/EventEnvelopeShould.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.envelope;
+
+import com.google.protobuf.Message;
+import io.spine.base.Event;
+import io.spine.base.EventId;
+import io.spine.test.TestEventFactory;
+import io.spine.test.event.ProjectCreated;
+import io.spine.test.event.ProjectId;
+import io.spine.type.EventClass;
+
+import static io.spine.base.Identifier.newUuid;
+
+/**
+ * @author Dmytro Dashenkov
+ */
+public class EventEnvelopeShould extends MessageEnvelopeShould<Event,
+        EventId,
+        EventEnvelope,
+        EventClass> {
+
+    private final TestEventFactory eventFactory =
+            TestEventFactory.newInstance(EventEnvelopeShould.class);
+
+    @Override
+    protected Event outerObject() {
+        final ProjectId projectId = ProjectId.newBuilder()
+                                             .setId(newUuid())
+                                             .build();
+        final Message eventMessage = ProjectCreated.newBuilder()
+                                                   .setProjectId(projectId)
+                                                   .build();
+        return eventFactory.createEvent(eventMessage);
+    }
+
+    @Override
+    protected EventEnvelope toEnvelope(Event obj) {
+        return EventEnvelope.of(obj);
+    }
+
+    @Override
+    protected EventClass getMessageClass(Event obj) {
+        return EventClass.of(obj);
+    }
+
+    @Override
+    protected EventId getId(Event obj) {
+        return obj.getId();
+    }
+}
--- client/src/test/java/io/spine/envelope/FailureEnvelopeShould.java
+++ client/src/test/java/io/spine/envelope/FailureEnvelopeShould.java
@@ -19,16 +19,16 @@
  */
 package io.spine.envelope;
 
-import com.google.common.testing.NullPointerTester;
 import com.google.protobuf.Int32Value;
 import com.google.protobuf.Message;
 import io.spine.base.Command;
 import io.spine.base.Failure;
+import io.spine.base.FailureId;
 import io.spine.base.Failures;
+import io.spine.protobuf.AnyPacker;
 import io.spine.test.TestActorRequestFactory;
 import io.spine.test.failures.Failures.CannotPerformBusinessOperation;
 import io.spine.type.FailureClass;
-import org.junit.Before;
 import org.junit.Test;
 
 import static io.spine.base.Identifier.newUuid;
@@ -37,52 +37,56 @@ import static org.junit.Assert.assertEquals;
 /**
  * @author Alex Tymchenko
  */
-public class FailureEnvelopeShould {
+public class FailureEnvelopeShould extends MessageEnvelopeShould<Failure,
+                                                                 FailureId,
+                                                                 FailureEnvelope,
+                                                                 FailureClass> {
 
     private final TestActorRequestFactory requestFactory =
             TestActorRequestFactory.newInstance(FailureEnvelopeShould.class);
 
-
-    private FailureEnvelope envelope;
-    private CannotPerformBusinessOperation failureMessage;
-    private Command command;
-    private Message commandMessage;
-
-    @Before
-    public void setUp() {
-        this.commandMessage = Int32Value.getDefaultInstance();
-        this.command = requestFactory.command().create(commandMessage);
-        this.failureMessage = CannotPerformBusinessOperation.newBuilder()
+    @Override
+    protected Failure outerObject() {
+        final Message commandMessage = Int32Value.getDefaultInstance();
+        final Command command = requestFactory.command().create(commandMessage);
+        final Message failureMessage = CannotPerformBusinessOperation.newBuilder()
                                                             .setOperationId(newUuid())
                                                             .build();
         final Failure failure = Failures.createFailure(failureMessage, command);
-        this.envelope = FailureEnvelope.of(failure);
+        return failure;
     }
 
-    @Test
-    public void pass_null_tolerance_check() {
-        new NullPointerTester()
-                .setDefault(Failure.class, Failure.getDefaultInstance())
-                .testAllPublicStaticMethods(FailureEnvelope.class);
+    @Override
+    protected FailureEnvelope toEnvelope(Failure obj) {
+        return FailureEnvelope.of(obj);
     }
 
-    @Test
-    public void obtain_failure_message() {
-        assertEquals(failureMessage, envelope.getMessage());
+    @Override
+    protected FailureClass getMessageClass(Failure obj) {
+        return FailureClass.of(obj);
     }
 
-    @Test
-    public void obtain_failure_message_class() {
-        assertEquals(FailureClass.of(this.failureMessage), envelope.getMessageClass());
+    @Override
+    protected FailureId getId(Failure obj) {
+        return obj.getId();
     }
 
     @Test
     public void obtain_command_context() {
+        final Failure failure = outerObject();
+        final Command command = failure.getContext()
+                                       .getCommand();
+        final FailureEnvelope envelope = toEnvelope(failure);
         assertEquals(command.getContext(), envelope.getCommandContext());
     }
 
     @Test
     public void obtain_command_message() {
+        final Failure failure = outerObject();
+        final Command command = failure.getContext()
+                                       .getCommand();
+        final Message commandMessage = AnyPacker.unpack(command.getMessage());
+        final FailureEnvelope envelope = toEnvelope(failure);
         assertEquals(commandMessage, envelope.getCommandMessage());
     }
 }
