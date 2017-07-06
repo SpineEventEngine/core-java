89b2eed494c6a2683e87c5b7ed79da0f2fe16aaf
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2017-07-06 19:02:59 +0300

    Improve Javadoc for `@Assign`

--- server/src/main/java/io/spine/server/command/Assign.java
+++ server/src/main/java/io/spine/server/command/Assign.java
@@ -28,23 +28,29 @@ import java.lang.annotation.Target;
 /**
  * Marks a method as command handler.
  *
- * <p>A command handler method:
+ * <p>A command handler method <strong>must</strong>:
  * <ul>
- *     <li>is annotated with {@link Assign};
- *     <li>has a default access modifier;
- *     <li>returns an event derived from {@link com.google.protobuf.Message Message} or
- *          a {@link java.util.List List} of messages;
- *     <li>accepts a command derived from {@link com.google.protobuf.Message Message}
- *          as a first parameter;
- *     <li>(optional) accepts a {@link io.spine.core.CommandContext CommandContext}
- *          as the second parameter.
+ *     <li>be annotated with {@link Assign};
+ *     <li>return an event derived from {@link com.google.protobuf.Message Message}
+ *     <strong>or</strong> a {@link java.util.List List} of messages;
+ *     <li>accept a command message derived from {@link com.google.protobuf.Message Message}
+ *         as the first parameter.
  * </ul>
  *
- * If the annotation is applied to a method which doesn't satisfy any of these requirements,
- * this method is not considered a command handler and is not registered for command dispatching.
+ * <p>If the annotation is applied to a method which doesn't satisfy any of these requirements,
+ * this method is not considered a command handler and is <strong>not</strong> registered for
+ * command dispatching.
  *
- * <p><b>IMPORTANT:</b> an application must have one and only one handler per command class.
- * Declaring two methods that handle the same command class will result in run-time error.
+ * <p>A command handler method <strong>should</strong> have package-private access. It will allow
+ * calling this method from tests. The method should not be {@code public} because it is not
+ * supposed to be called directly.
+ *
+ * <p>A command handler method <strong>may</strong> accept a {@link io.spine.core.CommandContext
+ * CommandContext} as the second parameter if handling of the command requires its context.
+ *
+ * <p><strong>IMPORTANT:</strong> an application must have one and only one handler per command
+ * message class. Declaring two methods that handle the same command class will result in run-time
+ * error.
  *
  * @author Alexander Yevsyukov
  */
