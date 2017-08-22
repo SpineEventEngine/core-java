24249c7ce3b22188975e5c89a64f924f3f2cec7d
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-08-22 15:37:38 +0300

    Get rid of silent fails during Maven artifacts publishing

--- scripts/publish-artifacts.sh
+++ scripts/publish-artifacts.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# This script uses Travis `TRAVIS_BRANCH` environment variable to check if the artifacts should be published.
+# This script uses Travis environment variables to check if the artifacts should be published.
 #
-# If the branch is `master`, script triggers the publishing process.
-# Tests are skipped during the publishing, as the script should be executed on `after_success` step of
-# the build lifecycle.
+# If the build is successful and the branch is `master`, script triggers the publishing process.
+#
+# Tests are skipped during the publishing, as the script should be executed after their execution.
 
 echo " -- PUBLISHING: current branch is $TRAVIS_BRANCH."
 
-if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if shouldPublish; then
     echo " ------ Publishing the artifacts to the repository..."
     ./gradlew publish -x test
     echo " ------ Artifacts published."
-else
-    echo " ------ Publishing is DISABLED for the current branch."
 fi
 
 echo " -- PUBLISHING: completed."
+
+shouldPublish() {
+    if [ "$TRAVIS_TEST_RESULT" == 1 ]; then
+        echo " ------ The build is broken. Publishing should not be performed."
+        return 0
+    fi
+
+    if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        echo " ------ Publishing is DISABLED for the current branch."
+        return 0
+    fi
+
+    return 1
+}
