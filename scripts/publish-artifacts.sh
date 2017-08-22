259d595e07ad631b9137879ad310f0cdcae11f8a
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-08-22 16:33:25 +0300

    Refactor `publish-artifacts.sh`

--- scripts/publish-artifacts.sh
+++ scripts/publish-artifacts.sh
@@ -8,18 +8,16 @@
 
 echo " -- PUBLISHING: current branch is $TRAVIS_BRANCH."
 
-if [ "$TRAVIS_TEST_RESULT" == 1 ]; then
-    echo " ------ The build is broken. Publishing will not be performed."
-    exit 0
-fi
-
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS_TEST_RESULT" == 0 ]; then
+        echo " ------ Publishing the artifacts to the repository..."
+        ./gradlew publish -x test
+        echo " ------ Artifacts published."
+    else
+        echo " ------ The build is broken. Publishing will not be performed."
+    fi
+else
     echo " ------ Publishing is DISABLED for the current branch."
-    exit 0
 fi
-
-echo " ------ Publishing the artifacts to the repository..."
-./gradlew publish -x test
-echo " ------ Artifacts published."
 
 echo " -- PUBLISHING: completed."
