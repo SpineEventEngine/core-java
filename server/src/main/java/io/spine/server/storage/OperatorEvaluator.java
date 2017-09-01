f15a521501787ffedec709856cb602139f528c98
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-09-01 16:50:25 +0300

    Mode `OperatorEvaluator` to higher package level.

--- server/src/main/java/io/spine/server/storage/OperatorEvaluator.java
+++ server/src/main/java/io/spine/server/storage/OperatorEvaluator.java
@@ -18,7 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.server.storage.memory;
+package io.spine.server.storage;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.Timestamp;
