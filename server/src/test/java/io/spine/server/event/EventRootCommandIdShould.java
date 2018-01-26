0937baae7494e46b9a3712387b5a7b7e60caff1d
mdrachuk <mdrachuk@projects.tm>
2018-01-26 19:49:29 +0200

    Create a test for events root command identifier to match the id of a command handled by an aggregate.

---
+++ server/src/test/java/io/spine/server/event/EventRootCommandIdShould.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.event;
+
+import com.google.protobuf.Message;
+import io.grpc.stub.StreamObserver;
+import io.spine.client.TestActorRequestFactory;
+import io.spine.core.Ack;
+import io.spine.core.Command;
+import io.spine.core.Event;
+import io.spine.server.BoundedContext;
+import io.spine.server.event.given.EventRootCommandIdTestEnv.ProjectAggregateRepository;
+import io.spine.server.event.given.EventRootCommandIdTestEnv.ResponseObserver;
+import io.spine.test.event.ProjectId;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.spine.grpc.StreamObservers.noOpObserver;
+import static io.spine.core.Events.getRootCommandId;
+import static io.spine.server.event.given.EventRootCommandIdTestEnv.createProject;
+import static io.spine.server.event.given.EventRootCommandIdTestEnv.newStreamObserver;
+import static io.spine.server.event.given.EventRootCommandIdTestEnv.newStreamQuery;
+import static io.spine.server.event.given.EventRootCommandIdTestEnv.projectId;
+import static org.junit.Assert.assertEquals;
+
+public class EventRootCommandIdShould {
+
+    private static final TestActorRequestFactory requestFactory =
+            TestActorRequestFactory.newInstance(EventRootCommandIdShould.class);
+
+    private static final ProjectId ID = projectId(EventRootCommandIdShould.class.getSimpleName());
+
+    private BoundedContext boundedContext;
+
+    @Before
+    public void setUp() {
+        boundedContext = BoundedContext.newBuilder()
+                                       .build();
+        final ProjectAggregateRepository repository = new ProjectAggregateRepository();
+        boundedContext.register(repository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        boundedContext.close();
+    }
+
+    @Test
+    public void match_the_id_of_a_command_handled_by_an_aggregate() {
+        final Command command = command(createProject(ID));
+        postCommand(command);
+
+        final List<Event> events = readEvents();
+
+        final Event event = events.get(0);
+        assertEquals(command.getId(), getRootCommandId(event));
+    }
+
+    @Test
+    public void match_the_id_of_a_command_handled_by_an_aggregate_for_multiple_events() {
+
+    }
+
+    @Test
+    public void match_the_id_of_an_external_event_handled_by_an_aggregate() {
+
+    }
+
+    @Test
+    public void match_the_id_of_a_command_handled_by_a_process_manager() {
+
+    }
+
+    @Test
+    public void match_the_id_of_a_command_handled_by_a_process_manager_for_multiple_events() {
+
+    }
+
+    @Test
+    public void match_the_id_of_an_external_event_handled_by_a_process_manager() {
+
+    }
+
+    private static Command command(Message message) {
+        return requestFactory.createCommand(message);
+    }
+
+    private void postCommand(Command command) {
+        final StreamObserver<Ack> observer = noOpObserver();
+        boundedContext.getCommandBus()
+                      .post(command, observer);
+    }
+
+    private List<Event> readEvents() {
+        final EventStreamQuery query = newStreamQuery();
+        final ResponseObserver observer = newStreamObserver();
+
+        boundedContext.getEventBus()
+                      .getEventStore()
+                      .read(query, observer);
+
+        final List<Event> results = observer.getResults();
+        return results;
+    }
+
+}
---
+++ server/src/test/java/io/spine/server/event/given/EventRootCommandIdTestEnv.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.event.given;
+
+import com.google.common.collect.ImmutableList;
+import io.grpc.stub.StreamObserver;
+import io.spine.core.CommandContext;
+import io.spine.core.Event;
+import io.spine.server.aggregate.Aggregate;
+import io.spine.server.aggregate.AggregateRepository;
+import io.spine.server.aggregate.Apply;
+import io.spine.server.command.Assign;
+import io.spine.server.event.EventStreamQuery;
+import io.spine.test.event.Project;
+import io.spine.test.event.ProjectCreated;
+import io.spine.test.event.ProjectId;
+import io.spine.test.event.ProjectVBuilder;
+import io.spine.test.event.command.CreateProject;
+
+import java.util.Collection;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Mykhailo Drachuk
+ */
+public class EventRootCommandIdTestEnv {
+
+    private EventRootCommandIdTestEnv() {
+        // Prevent instantiation.
+    }
+
+    public static ProjectId projectId(String id) {
+        checkNotNull(id);
+
+        return ProjectId.newBuilder()
+                        .setId(id)
+                        .build();
+    }
+
+    public static CreateProject createProject(ProjectId id) {
+        checkNotNull(id);
+
+        return CreateProject.newBuilder()
+                            .setProjectId(id)
+                            .build();
+    }
+
+    public static EventStreamQuery newStreamQuery() {
+        return EventStreamQuery.newBuilder()
+                               .build();
+    }
+
+    public static ResponseObserver newStreamObserver() {
+        return new ResponseObserver();
+    }
+
+    /**
+     * A stream observer for store querying.
+     */
+    public static class ResponseObserver implements StreamObserver<Event> {
+
+        private final Collection<Event> resultStorage;
+
+        private ResponseObserver() {
+            this.resultStorage = newArrayList();
+        }
+
+        @Override
+        public void onNext(Event value) {
+            resultStorage.add(value);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            fail(t.getMessage());
+        }
+
+        @Override
+        public void onCompleted() {
+            // Do nothing.
+        }
+
+        public List<Event> getResults() {
+            return ImmutableList.copyOf(resultStorage);
+        }
+    }
+
+    public static class ProjectAggregateRepository
+            extends AggregateRepository<ProjectId, ProjectAggregate> { }
+
+    static class ProjectAggregate
+            extends Aggregate<ProjectId, Project, ProjectVBuilder> {
+
+        private ProjectAggregate(ProjectId id) {
+            super(id);
+        }
+
+        private static ProjectCreated projectCreated(ProjectId projectId) {
+            return ProjectCreated.newBuilder()
+                                 .setProjectId(projectId)
+                                 .build();
+        }
+
+        @Assign
+        ProjectCreated on(CreateProject cmd, CommandContext ctx) {
+            final ProjectCreated event = projectCreated(cmd.getProjectId());
+            return event;
+        }
+
+        @Apply
+        private void event(ProjectCreated event) {
+            getBuilder()
+                    .setId(event.getProjectId())
+                    .setStatus(Project.Status.CREATED);
+        }
+    }
+}
