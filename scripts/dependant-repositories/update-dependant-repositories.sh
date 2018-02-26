328a294a31a55689d161c9334578687a1f9e7336
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-02-26 13:26:30 +0200

    Add description of the GitHub token usage

--- scripts/dependant-repositories/update-dependant-repositories.sh
+++ scripts/dependant-repositories/update-dependant-repositories.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #
 # Make all repositories dependant on the core library have its latest version as the dependency.
+#
+# This script accept a single command line parameter - "gitAuthorizationToken".
+# Such token can be generated in the "Settings/Developer settings/Personal access tokens" page on the GitHub website.
+# It allows to perform all requests to GitHub API on behalf of the token owner.
+#
+# To store such token for the usage in the ".travis.yml" file, call 'travis encrypt GITHUB_TOKEN=*your token*'.
+# Then copy-paste the result in the "env: global" section of the ".travis.yml" in place of the old encrypted token.
+# The decrypted version of the token will be available in the GITHUB_TOKEN environment variable during the build.
 
 function main() {
     local gitAuthorizationToken="$1"
