082bdc06c4f3da378159f6dd7d692124cfe5dff1
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2018-04-16 21:08:28 +0300

    Document the `Sharding` interface.

--- server/src/main/java/io/spine/server/delivery/Sharding.java
+++ server/src/main/java/io/spine/server/delivery/Sharding.java
@@ -25,17 +25,59 @@ import io.spine.core.MessageEnvelope;
 import java.util.Set;
 
 /**
+ * The sharding service.
+ *
+ * <p>Responsible for orchestrating the shardables and their inbound streams of messages.
+ *
+ * <p>Typically is a JVM-wide singleton.
+ *
  * @author Alex Tymchenko
  */
 public interface Sharding {
 
+    /**
+     * Registers a shardable instance.
+     *
+     * <p>After the registration the shardable starts to be served with the messages targeting the
+     * instances which belong to its shard.
+     *
+     * @param shardable a shardable to register
+     */
     void register(Shardable shardable);
 
+    /**
+     * Unregister a shardable instance.
+     *
+     * <p>After this operation, the messages are no longer dispatched to the shards of this
+     * shardable.
+     *
+     * <p>Has no effect if there is no such a shardable registered.
+     *
+     * @param shardable a shardable instance to unregister
+     */
     void unregister(Shardable shardable);
 
+    /**
+     * Defines the sharding keys for the given shardable to be served within this
+     * {@code Sharding instance}.
+     *
+     * @param shardable the shardable to define the keys for
+     * @param keys all the sharding keys to pick those to handle within this instance
+     * @return
+     */
     @SPI
     Set<ShardingKey> pickKeysForNode(Shardable shardable, Set<ShardingKey> keys);
 
+    /**
+     * Finds the message streams which delivery tag and target entity identifier
+     * match the specified values.
+     *
+     * @param tag the delivery tag
+     * @param targetId the identifier of the target entity
+     * @param <I> the type of target entity identifiers
+     * @param <E> the type of message envelope to find the message stream for
+     * @return the message streams matching the search criteria
+     */
     <I, E extends MessageEnvelope<?, ?, ?>> Set<ShardedStream<I, ?, E>>
     find(DeliveryTag<E> tag, I targetId);
 
