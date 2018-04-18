5611a38a4422cf2fd7ffe167ef86d7a4bdde6dd2
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2018-04-18 20:55:51 +0300

    Enhance the image caption location and text.

--- server/src/main/java/io/spine/server/delivery/package-info.java
+++ server/src/main/java/io/spine/server/delivery/package-info.java
@@ -57,9 +57,9 @@
  *
  * <h3>Flow</h3>
  *
- * <i>(illustrated for Commands, however Events and Rejections are dispatched similarly)</i>
- *
  * <p><img src="./doc-files/sharding-commands-flow.png" alt="Sharding command flow">
+ *
+ * <i>Command flow. Events and Rejections are dispatched similarly.</i>
  *
  * <p>In this flow the message channel serves as a single-lane road, enqueuing the messages
  * from multiple senders (potentially, posting their messages simultaneously) and transferring
