fa8760d63bd46469a138eee632f388ff636c16fe
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-05-11 21:03:49 +0300

    Correct alignment of arguments in doc

--- server/src/main/java/io/spine/server/entity/storage/EnumConverters.java
+++ server/src/main/java/io/spine/server/entity/storage/EnumConverters.java
@@ -14,7 +14,7 @@
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USEC
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -39,7 +39,7 @@ final class EnumConverters {
     /**
      * Creates a {@link EnumConverter} for the given {@link EnumType} and {@link Class source type}.
      *
-     * @param enumType the {@code EnumType} which defines the conversion method
+     * @param enumType   the {@code EnumType} which defines the conversion method
      * @param sourceType the source {@code Class} of the conversion
      * @return the converter for the given enum type and the source class
      */
