149b0e183f57a2b6a9fd76abbd7286d448e60613
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-06-12 12:23:19 +0300

    Introduce nested class

--- server/src/test/java/io/spine/server/aggregate/AggregateMessageDeliveryTest.java
+++ server/src/test/java/io/spine/server/aggregate/AggregateMessageDeliveryTest.java
@@ -33,6 +33,7 @@ import io.spine.server.delivery.given.ThreadStats;
 import io.spine.test.aggregate.ProjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static io.spine.server.aggregate.given.AggregateMessageDeliveryTestEnv.cannotStartProject;
@@ -43,6 +44,9 @@ import static io.spine.server.delivery.given.MessageDeliveryTestEnv.dispatchWait
 /**
  * @author Alex Tymchenko
  */
+@SuppressWarnings({"InnerClassMayBeStatic", "ClassCanBeStatic"
+        /* JUnit 5 Nested classes cannot to be static. */,
+        "DuplicateStringLiteralInspection" /* Common test display names */})
 @DisplayName("Aggregate Message delivery should")
 class AggregateMessageDeliveryTest extends AbstractMessageDeliveryShould {
 
@@ -54,158 +58,174 @@ class AggregateMessageDeliveryTest extends AbstractMessageDeliveryShould {
                        .clear();
     }
 
-    @Test
-    @DisplayName("dispatch commands to single shard in multithreaded env")
-    void dispatchCommandsToShard() throws Exception {
-        final ParallelDispatcher<ProjectId, Command> dispatcher =
-                new ParallelDispatcher<ProjectId, Command>(
-                        42, 400, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
+    @Nested
+    @DisplayName("in multithreaded env, dispatch commands")
+    class DispatchCommands {
 
-                    @Override
-                    protected Command newMessage() {
-                        return startProject();
-                    }
+        @Test
+        @DisplayName("to single shard")
+        void toSingleShard() throws Exception {
+            final ParallelDispatcher<ProjectId, Command> dispatcher =
+                    new ParallelDispatcher<ProjectId, Command>(
+                            42, 400, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
 
-                    @Override
-                    protected void postToBus(BoundedContext context, Command command) {
-                        context.getCommandBus()
-                               .post(command, StreamObservers.noOpObserver());
-                    }
-                };
+                        @Override
+                        protected Command newMessage() {
+                            return startProject();
+                        }
 
-        dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
+                        @Override
+                        protected void postToBus(BoundedContext context, Command command) {
+                            context.getCommandBus()
+                                   .post(command, StreamObservers.noOpObserver());
+                        }
+                    };
+
+            dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
+        }
+
+        @Test
+        @DisplayName("to several shards")
+        void toSeveralShards() throws Exception {
+            final ParallelDispatcher<ProjectId, Command> dispatcher =
+                    new ParallelDispatcher<ProjectId, Command>(
+                            23, 423, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
+
+                        @Override
+                        protected Command newMessage() {
+                            return startProject();
+                        }
+
+                        @Override
+                        protected void postToBus(BoundedContext context, Command command) {
+                            context.getCommandBus()
+                                   .post(command, StreamObservers.noOpObserver());
+                        }
+                    };
+            dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
+        }
+
     }
 
-    @Test
-    @DisplayName("dispatch events to single shard in multithreaded env")
-    void dispatchEventsToShard() throws Exception {
-        final ParallelDispatcher<ProjectId, Event> dispatcher =
-                new ParallelDispatcher<ProjectId, Event>(
-                        130, 500, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
+    @Nested
+    @DisplayName("in multithreaded env, dispatch events")
+    class DispatchEvents {
 
-                    @Override
-                    protected Event newMessage() {
-                        return projectCancelled();
-                    }
+        @Test
+        @DisplayName("to single shard")
+        void toSingleShard() throws Exception {
+            final ParallelDispatcher<ProjectId, Event> dispatcher =
+                    new ParallelDispatcher<ProjectId, Event>(
+                            130, 500, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
 
-                    @Override
-                    protected void postToBus(BoundedContext context, Event event) {
-                        context.getEventBus()
-                               .post(event, StreamObservers.noOpObserver());
-                    }
-                };
+                        @Override
+                        protected Event newMessage() {
+                            return projectCancelled();
+                        }
 
-        dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
+                        @Override
+                        protected void postToBus(BoundedContext context, Event event) {
+                            context.getEventBus()
+                                   .post(event, StreamObservers.noOpObserver());
+                        }
+                    };
+
+            dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
+        }
+
+        @Test
+        @DisplayName("to several shards")
+        void toSeveralShards() throws Exception {
+            final ParallelDispatcher<ProjectId, Event> dispatcher =
+                    new ParallelDispatcher<ProjectId, Event>(
+                            190, 900, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
+
+                        @Override
+                        protected Event newMessage() {
+                            return projectCancelled();
+                        }
+
+                        @Override
+                        protected void postToBus(BoundedContext context, Event event) {
+                            context.getEventBus()
+                                   .post(event, StreamObservers.noOpObserver());
+                        }
+                    };
+
+            dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
+        }
     }
 
-    @Test
-    @DisplayName("dispatch rejections to single shard in multithreaded env")
-    void dispatchRejectionsToShard() throws Exception {
-        final ParallelDispatcher<ProjectId, Rejection> dispatcher =
-                new ParallelDispatcher<ProjectId, Rejection>(
-                        36, 12, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
+    @Nested
+    @DisplayName("in multithreaded env, dispatch rejections")
+    class DispatchRejections {
 
-                    @Override
-                    protected Rejection newMessage() {
-                        return cannotStartProject();
-                    }
+        @Test
+        @DisplayName("to single shard")
+        void dispatchRejectionsToShard() throws Exception {
+            final ParallelDispatcher<ProjectId, Rejection> dispatcher =
+                    new ParallelDispatcher<ProjectId, Rejection>(
+                            36, 12, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
 
-                    @Override
-                    protected void postToBus(BoundedContext context, Rejection rejection) {
-                        context.getRejectionBus()
-                               .post(rejection, StreamObservers.noOpObserver());
-                    }
-                };
+                        @Override
+                        protected Rejection newMessage() {
+                            return cannotStartProject();
+                        }
 
-        dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
-    }
+                        @Override
+                        protected void postToBus(BoundedContext context, Rejection rejection) {
+                            context.getRejectionBus()
+                                   .post(rejection, StreamObservers.noOpObserver());
+                        }
+                    };
 
-    @Test
-    @DisplayName("dispatch commands to several shards in multithreaded env")
-    void dispatchCommandsToSeveralShards() throws Exception {
-        final ParallelDispatcher<ProjectId, Command> dispatcher =
-                new ParallelDispatcher<ProjectId, Command>(
-                        23, 423, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
+            dispatcher.dispatchMessagesTo(new SingleShardProjectRepository());
+        }
 
-                    @Override
-                    protected Command newMessage() {
-                        return startProject();
-                    }
+        @Test
+        @DisplayName("to several shards")
+        void dispatchRejectionsToSeveralShards() throws Exception {
+            final ParallelDispatcher<ProjectId, Rejection> dispatcher =
+                    new ParallelDispatcher<ProjectId, Rejection>(
+                            40, 603, dispatchWaitTime()) {
+                        @Override
+                        protected ThreadStats<ProjectId> getStats() {
+                            return DeliveryProject.getStats();
+                        }
 
-                    @Override
-                    protected void postToBus(BoundedContext context, Command command) {
-                        context.getCommandBus()
-                               .post(command, StreamObservers.noOpObserver());
-                    }
-                };
-        dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
-    }
+                        @Override
+                        protected Rejection newMessage() {
+                            return cannotStartProject();
+                        }
 
-    @Test
-    @DisplayName("dispatch events to several shards in multithreaded env")
-    void dispatchEventsToSeveralShards() throws Exception {
-        final ParallelDispatcher<ProjectId, Event> dispatcher =
-                new ParallelDispatcher<ProjectId, Event>(
-                        190, 900, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
+                        @Override
+                        protected void postToBus(BoundedContext context, Rejection rejection) {
+                            context.getRejectionBus()
+                                   .post(rejection, StreamObservers.noOpObserver());
+                        }
+                    };
 
-                    @Override
-                    protected Event newMessage() {
-                        return projectCancelled();
-                    }
-
-                    @Override
-                    protected void postToBus(BoundedContext context, Event event) {
-                        context.getEventBus()
-                               .post(event, StreamObservers.noOpObserver());
-                    }
-                };
-
-        dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
-    }
-
-    @Test
-    @DisplayName("dispatch rejections to several shards in multithreaded env")
-    void dispatchRejectionsToSeveralShards() throws Exception {
-        final ParallelDispatcher<ProjectId, Rejection> dispatcher =
-                new ParallelDispatcher<ProjectId, Rejection>(
-                        40, 603, dispatchWaitTime()) {
-                    @Override
-                    protected ThreadStats<ProjectId> getStats() {
-                        return DeliveryProject.getStats();
-                    }
-
-                    @Override
-                    protected Rejection newMessage() {
-                        return cannotStartProject();
-                    }
-
-                    @Override
-                    protected void postToBus(BoundedContext context, Rejection rejection) {
-                        context.getRejectionBus()
-                               .post(rejection, StreamObservers.noOpObserver());
-                    }
-                };
-
-        dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
+            dispatcher.dispatchMessagesTo(new TripleShardProjectRepository());
+        }
     }
 }
