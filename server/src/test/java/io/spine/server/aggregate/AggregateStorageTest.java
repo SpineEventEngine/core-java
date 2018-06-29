8164a5791accdf63f67fbb8971da03ae38cf4672
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-06-29 19:48:21 +0300

    Fix compilation error

--- server/src/test/java/io/spine/server/aggregate/AggregateStorageTest.java
+++ server/src/test/java/io/spine/server/aggregate/AggregateStorageTest.java
@@ -145,7 +145,7 @@ public abstract class AggregateStorageTest
             "RefusedBequest",
             "MethodDoesntCallSuperMethod"
     })
-    protected void rewriteRecordIfWriteByTheSameId() {
+    protected void rewriteRecord() {
     }
 
     /**
