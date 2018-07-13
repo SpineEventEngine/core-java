0c6bd7f07b20f7af179070aaabe58f0be29c3859
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-07-13 18:19:11 +0300

    Fix import after merge.

--- server/src/test/java/io/spine/system/server/EntityHistoryTest.java
+++ server/src/test/java/io/spine/system/server/EntityHistoryTest.java
@@ -74,7 +74,9 @@ import static io.spine.server.storage.memory.InMemoryStorageFactory.newInstance;
 import static io.spine.util.Exceptions.newIllegalStateException;
 import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Dmytro Dashenkov
