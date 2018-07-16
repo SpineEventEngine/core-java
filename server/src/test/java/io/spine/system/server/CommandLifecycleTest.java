0e0d04bea283ff8d84f345d39a15e18751753e55
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-07-16 14:46:29 +0300

    Create test suite class for the CommandLifecycle tests.

---
+++ server/src/test/java/io/spine/system/server/CommandLifecycleTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.system.server;
+
+import io.spine.core.BoundedContextName;
+import io.spine.server.BoundedContext;
+import io.spine.testing.client.TestActorRequestFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.spine.server.SystemBoundedContexts.systemOf;
+import static io.spine.server.storage.memory.InMemoryStorageFactory.newInstance;
+
+/**
+ * @author Dmytro Dashenkov
+ */
+@SuppressWarnings("InnerClassMayBeStatic")
+@DisplayName("CommandLifecycle should")
+class CommandLifecycleTest {
+
+    private static final TestActorRequestFactory requestFactory =
+            TestActorRequestFactory.newInstance(EntityHistoryTest.class);
+
+    private BoundedContext context;
+    private BoundedContext system;
+
+    @BeforeEach
+    void setUp() {
+        BoundedContextName contextName = BoundedContextName
+                .newBuilder()
+                .setValue(EntityHistoryTest.class.getSimpleName())
+                .build();
+        context = BoundedContext
+                .newBuilder()
+                .setName(contextName)
+                .setStorageFactorySupplier(() -> newInstance(contextName, false))
+                .build();
+        system = systemOf(context);
+    }
+
+    @DisplayName("produce system events when")
+    class ProduceEvents {
+
+        @Test
+        @DisplayName("command is received")
+        void received() {
+
+        }
+    }
+}
