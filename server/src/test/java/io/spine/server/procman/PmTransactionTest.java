218cd34db001f5eec1d85831ea7ee8b2fe238b07
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-07-24 20:34:48 +0300

    Optimize imports

--- server/src/test/java/io/spine/server/procman/PmTransactionTest.java
+++ server/src/test/java/io/spine/server/procman/PmTransactionTest.java
@@ -32,9 +32,6 @@ import io.spine.test.procman.ProjectId;
 import io.spine.test.procman.event.PmProjectCreated;
 import io.spine.test.procman.event.PmTaskAdded;
 import io.spine.validate.ConstraintViolation;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
