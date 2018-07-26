7959efec36b645244068bb63817d7b7cc8c7400b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-07-27 00:49:58 +0300

    Fix Javadoc language

--- server/src/main/java/io/spine/server/rejection/model/RejectionSubscriberMethod.java
+++ server/src/main/java/io/spine/server/rejection/model/RejectionSubscriberMethod.java
@@ -74,7 +74,7 @@ public final class RejectionSubscriberMethod
 
     /**
      * @implNote This method is never called because
-     * {@link #invoke(Object, Message, RejectionContext) invoke()} overriden in this class
+     * {@link #invoke(Object, Message, RejectionContext) invoke()} overridden in this class
      * does not call
      * {@linkplain io.spine.server.model.HandlerMethod#invoke(Object, Message, Message)
      * the parent method} (which invokes {@code toResult()}).
