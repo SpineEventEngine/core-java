f1b153d31c7ee7cef31b54bd498e57c9d19d6fe4
illia-shepilov <illia.shepilov@teamdev.com>
2018-07-27 13:48:20 +0200

    Remove unused import.

--- server/src/main/java/io/spine/server/event/ERepository.java
+++ server/src/main/java/io/spine/server/event/ERepository.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Lists.newArrayList;
