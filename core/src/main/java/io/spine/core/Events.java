cd37ca5e2467026b99ba42ec0f480c797a855d6a
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-08-10 17:05:28 +0300

    Remove unused method.

--- core/src/main/java/io/spine/core/Events.java
+++ core/src/main/java/io/spine/core/Events.java
@@ -216,20 +216,6 @@ public final class Events {
     }
 
     /**
-     * Tells whether the passed message class represents a rejection message.
-     */
-    public static boolean isRejection(Class<? extends Message> messageClass) {
-        checkNotNull(messageClass);
-        Class<?> enclosingClass = messageClass.getEnclosingClass();
-        if (enclosingClass == null) {
-            return false; // Rejection messages are generated as inner static classes.
-        }
-        boolean hasCorrectSuffix = enclosingClass.getName()
-                                                 .endsWith(REJECTION_CLASS_SUFFIX);
-        return hasCorrectSuffix;
-    }
-
-    /**
      * Constructs a new {@link RejectionEventContext} from the given command message and
      * {@link ThrowableMessage}.
      *
