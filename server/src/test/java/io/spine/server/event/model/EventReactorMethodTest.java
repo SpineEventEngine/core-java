3e3f7171faea2d0f6f2168182ac6362d436749bb
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-08-16 01:32:41 +0300

    Test wrong reactor method params

--- server/src/test/java/io/spine/server/event/model/EventReactorMethodTest.java
+++ server/src/test/java/io/spine/server/event/model/EventReactorMethodTest.java
@@ -20,13 +20,25 @@
 
 package io.spine.server.event.model;
 
+import com.google.common.truth.IterableSubject;
+import io.spine.core.EventContext;
+import io.spine.server.event.EventReactor;
 import io.spine.server.event.model.given.reactor.RcIterableReturn;
 import io.spine.server.event.model.given.reactor.RcOneParam;
 import io.spine.server.event.model.given.reactor.RcReturnOptional;
 import io.spine.server.event.model.given.reactor.RcTwoParams;
 import io.spine.server.event.model.given.reactor.RcWrongAnnotation;
+import io.spine.server.event.model.given.reactor.RcWrongFirstParam;
 import io.spine.server.event.model.given.reactor.RcWrongNoAnnotation;
 import io.spine.server.event.model.given.reactor.RcWrongNoParam;
+import io.spine.server.event.model.given.reactor.RcWrongSecondParam;
+import io.spine.server.event.model.given.reactor.TestEventReactor;
+import io.spine.server.model.MethodFactory;
+import io.spine.server.model.ReactorMethodResult;
+import io.spine.test.reflect.ProjectId;
+import io.spine.test.reflect.event.RefProjectCreated;
+import io.spine.test.reflect.event.RefProjectStarted;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -35,6 +47,7 @@ import java.lang.reflect.Method;
 import java.util.function.Predicate;
 
 import static com.google.common.truth.Truth.assertThat;
+import static io.spine.testing.TestValues.randomString;
 
 /**
  * @author Alexander Yevsyukov
@@ -43,7 +56,8 @@ import static com.google.common.truth.Truth.assertThat;
 @DisplayName("EventReactorMethod should")
 class EventReactorMethodTest {
 
-    private static final Predicate<Method> predicate = EventReactorMethod.factory().getPredicate();
+    private static final MethodFactory<EventReactorMethod> factory = EventReactorMethod.factory();
+    private static final Predicate<Method> predicate = factory.getPredicate();
 
     private static void assertValid(Method reactor, boolean isReactor) {
         assertThat(predicate.test(reactor)).isEqualTo(isReactor);
@@ -69,28 +83,93 @@ class EventReactorMethodTest {
     }
 
     @Nested
-    @DisplayName("allow return value of type")
-    class ReturnValues {
+    @DisplayName("support Message return type")
+    class MessageReturn {
 
         @Test
-        @DisplayName("Message")
-        void messageReturn() {
+        @DisplayName("in predicate")
+        void predicate() {
             Method method = new RcOneParam().getMethod();
             assertValid(method, true);
         }
 
+    }
+
+    @Nested
+    @DisplayName("support Iterable return type")
+    class ReturnValues {
+
         @Test
-        @DisplayName("Iterable")
-        void iterableReturn() {
+        @DisplayName("in predicate")
+        void predicate() {
             Method method = new RcIterableReturn().getMethod();
             assertValid(method, true);
         }
+    }
+
+    @Nested
+    @DisplayName("support Optional return type")
+    class OptionalReturn {
+
+        private EventReactor target;
+        private Method rawMethod;
+        private EventReactorMethod method;
+
+        @BeforeEach
+        void setUp() {
+            target = new RcReturnOptional();
+            rawMethod = ((TestEventReactor) target).getMethod();
+            method = factory.create(rawMethod);
+        }
 
         @Test
-        @DisplayName("Optional")
-        void optionalReturn() {
-            Method method = new RcReturnOptional().getMethod();
-            assertValid(method, true);
+        @DisplayName("in predicate")
+        void inPredicate() {
+            assertValid(rawMethod, true);
+        }
+
+        @Test
+        @DisplayName("in factory")
+        void inFactory() {
+            assertThat(method).isNotNull();
+        }
+
+        @Test
+        @DisplayName("when returning value")
+        void returnValue() {
+            ProjectId id = ProjectId
+                    .newBuilder()
+                    .setId(randomString())
+                    .build();
+            RefProjectCreated event = RefProjectCreated
+                    .newBuilder()
+                    .setProjectId(id)
+                    .build();
+
+            ReactorMethodResult result =
+                    method.invoke(target, event, EventContext.getDefaultInstance());
+
+            IterableSubject assertThat = assertThat(result.asMessages());
+            assertThat.hasSize(1);
+            assertThat.containsExactly(
+                    RefProjectStarted.newBuilder()
+                                     .setProjectId(id)
+                                     .build()
+            );
+        }
+
+        @Test
+        @DisplayName("when returning Optional.empty()")
+        void returnEmpty() {
+            // Passing event without projectId should return `Optional.empty()`.
+            RefProjectCreated event = RefProjectCreated
+                    .newBuilder()
+                    .build();
+
+            ReactorMethodResult result =
+                    method.invoke(target, event, EventContext.getDefaultInstance());
+
+            assertThat(result.asMessages()).isEmpty();
         }
     }
 
@@ -116,6 +195,20 @@ class EventReactorMethodTest {
         @DisplayName("it has no parameters")
         void noParameters() {
             Method method = new RcWrongNoParam().getMethod();
+            assertValid(method, false);
+        }
+
+        @Test
+        @DisplayName("the first parameter is not Message")
+        void notMessageParam() {
+            Method method = new RcWrongFirstParam().getMethod();
+            assertValid(method, false);
+        }
+
+        @Test
+        @DisplayName("the second parameter is not EventContext")
+        void notContextParam() {
+            Method method = new RcWrongSecondParam().getMethod();
             assertValid(method, false);
         }
     }
