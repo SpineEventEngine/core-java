177969b8ed4fc7fa94b1ae0ba173eb2042c62f8d
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-09-24 02:11:47 +0300

    Optimize import

--- core/src/main/java/io/spine/core/EventClass.java
+++ core/src/main/java/io/spine/core/EventClass.java
@@ -26,7 +26,6 @@ import com.google.protobuf.Message;
 import io.spine.type.MessageClass;
 
 import java.util.Arrays;
-import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
