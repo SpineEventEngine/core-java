4a408f15283e5473eca8e5a2caf03b8ff1999726
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-12-09 21:36:07 +0200

    Use backticks in error log

--- server/src/main/java/io/spine/server/aggregate/AggregateRepository.java
+++ server/src/main/java/io/spine/server/aggregate/AggregateRepository.java
@@ -360,7 +360,7 @@ public abstract class AggregateRepository<I, A extends Aggregate<I, ?, ?>>
     public void onError(EventEnvelope envelope, RuntimeException exception) {
         checkNotNull(envelope);
         checkNotNull(exception);
-        logError("Error reacting on event (class: %s id: %s) in aggregate of type %s.",
+        logError("Error reacting on event (class: `%s` id: `%s`) in aggregate of type `%s.`",
                  envelope, exception);
     }
 
