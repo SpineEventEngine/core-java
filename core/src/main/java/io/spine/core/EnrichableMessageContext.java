ecebbb0ace0c4a1b7bee2445f1576e32ba33645f
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-04-12 16:31:06 +0300

    Mark interface immutable

--- core/src/main/java/io/spine/core/EnrichableMessageContext.java
+++ core/src/main/java/io/spine/core/EnrichableMessageContext.java
@@ -20,6 +20,7 @@
 
 package io.spine.core;
 
+import com.google.errorprone.annotations.Immutable;
 import com.google.protobuf.Message;
 import io.spine.annotation.GeneratedMixin;
 import io.spine.base.MessageContext;
@@ -33,6 +34,7 @@ import static io.spine.core.Enrichments.containerIn;
  * A common interface for message contexts that hold enrichments.
  */
 @GeneratedMixin
+@Immutable
 public interface EnrichableMessageContext extends MessageContext {
 
     /**
