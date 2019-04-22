45ef46dcac4599765967417f7d99f742231fce1e
Yuri Serhiichuk <yuri.sergiichuk@teamdev.com>
2019-04-22 11:58:04 +0300

    Move command handler test env to closer given package

--- server/src/test/java/io/spine/server/commandbus/given/CommandHandlerTestEnv.java
+++ server/src/test/java/io/spine/server/commandbus/given/CommandHandlerTestEnv.java
@@ -18,7 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.server.event.given;
+package io.spine.server.commandbus.given;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
