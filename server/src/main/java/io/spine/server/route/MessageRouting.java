8c8b80aa7dd2c70a6dc79767451a4d776ec0688e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-04-27 00:21:50 +0300

    Improve code layout

--- server/src/main/java/io/spine/server/route/MessageRouting.java
+++ server/src/main/java/io/spine/server/route/MessageRouting.java
@@ -137,9 +137,10 @@ abstract class MessageRouting<M extends Message, C extends Message, R> implement
     public void remove(Class<? extends M> messageClass) {
         checkNotNull(messageClass);
         if (!routes.containsKey(messageClass)) {
-            throw newIllegalStateException("Cannot remove the route for the message class (%s): " +
-                                                   "a custom route was not previously set.",
-                                           messageClass.getName());
+            throw newIllegalStateException(
+                    "Cannot remove the route for the message class (`%s`): " +
+                            "a custom route was not previously set.",
+                    messageClass.getName());
         }
         routes.remove(messageClass);
     }
