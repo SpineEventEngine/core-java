0ab96e7dc4e20c1042f5a26305f6d780641eec29
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-07-01 15:03:22 +0300

    Resolve cyclic dependency between `Delivery` and `ServerEnvironment`. Access `Delivery` in synchronized manner only to avoid issues in a multi-threaded environment.

--- server/src/main/java/io/spine/server/ServerEnvironment.java
+++ server/src/main/java/io/spine/server/ServerEnvironment.java
@@ -30,6 +30,7 @@ import io.spine.server.delivery.Delivery;
 import io.spine.server.storage.StorageFactory;
 import io.spine.server.storage.memory.InMemoryStorageFactory;
 import io.spine.server.trace.TracerFactory;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Optional;
@@ -76,9 +77,10 @@ public final class ServerEnvironment implements AutoCloseable {
      * The strategy of delivering the messages received by entity repositories
      * to the entity instances.
      *
-     * <p>By default, initialized with the {@linkplain Delivery#local() local} delivery.
+     * <p>If not {@linkplain #configureDelivery(Delivery) configured by the end-user},
+     * initialized with the {@linkplain Delivery#local() local} delivery by default.
      */
-    private Delivery delivery;
+    private @MonotonicNonNull Delivery delivery;
 
     /**
      * The storage factory for the production mode of the application.
@@ -96,7 +98,6 @@ public final class ServerEnvironment implements AutoCloseable {
     private Supplier<CommandScheduler> commandScheduler;
 
     private ServerEnvironment() {
-        delivery = Delivery.local();
         nodeId = NodeId.newBuilder()
                        .setValue(Identifier.newUuid())
                        .vBuild();
@@ -150,7 +151,7 @@ public final class ServerEnvironment implements AutoCloseable {
      * even dangerous to update the delivery mechanism later when the message delivery
      * process may have been already used by various {@code BoundedContext}s.
      */
-    public void configureDelivery(Delivery delivery) {
+    public synchronized void configureDelivery(Delivery delivery) {
         checkNotNull(delivery);
         this.delivery = delivery;
     }
@@ -161,7 +162,10 @@ public final class ServerEnvironment implements AutoCloseable {
      * <p>Unless {@linkplain #configureDelivery(Delivery) updated manually}, returns
      * a {@linkplain Delivery#local() local implementation} of {@code Delivery}.
      */
-    public Delivery delivery() {
+    public synchronized Delivery delivery() {
+        if(delivery == null) {
+            delivery = Delivery.local();
+        }
         return delivery;
     }
 
