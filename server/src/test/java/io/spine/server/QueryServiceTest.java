5a3068563d00a4e94ac74d178377365027bf1fc0
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-07-18 19:02:00 +0300

    Fix mock construction

--- server/src/test/java/io/spine/server/QueryServiceTest.java
+++ server/src/test/java/io/spine/server/QueryServiceTest.java
@@ -47,6 +47,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
@@ -137,7 +138,7 @@ class QueryServiceTest {
     @MuteLogging
     @DisplayName("return error if query failed to execute")
     void returnErrorOnQueryFail() {
-        when(projectDetailsRepository.loadAllRecords(ResponseFormat.getDefaultInstance()))
+        when(projectDetailsRepository.loadAllRecords(any(ResponseFormat.class)))
                 .thenThrow(RuntimeException.class);
         Query query = Given.AQuery.readAllProjects();
         service.read(query, responseObserver);
