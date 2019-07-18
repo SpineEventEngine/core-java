cf3ec1741177dfd63927d0e96fba68b936331639
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-07-18 13:57:34 +0300

    Check that filters are supplied if not explicitly querying for all entities

--- server/src/main/java/io/spine/server/stand/EntityQueryProcessor.java
+++ server/src/main/java/io/spine/server/stand/EntityQueryProcessor.java
@@ -33,8 +33,10 @@ import io.spine.server.entity.RecordBasedRepository;
 
 import java.util.Iterator;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.Streams.stream;
+import static io.spine.validate.Validate.isNotDefault;
 
 /**
  * Processes the queries targeting {@link io.spine.server.entity.Entity Entity} objects.
@@ -62,6 +64,8 @@ class EntityQueryProcessor implements QueryProcessor {
     private Iterator<EntityRecord> loadByQuery(Query query) {
         Target target = query.getTarget();
         TargetFilters filters = target.getFilters();
+        checkArgument(isNotDefault(filters),
+                      "`Target.filters` are empty but `Target.include_all` is not set.");
         Iterator<EntityRecord> entities = repository.findRecords(filters, query.getFormat());
         return entities;
     }
