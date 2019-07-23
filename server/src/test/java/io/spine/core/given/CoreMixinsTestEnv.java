7bf846cdaddf758f7e4e0a2675b0d78dfa0a6025
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-07-23 17:33:44 +0300

    Add mixins for the dispatching messages and test that their fields are reachable.

---
+++ server/src/test/java/io/spine/core/given/CoreMixinsTestEnv.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.core.given;
+
+import com.google.protobuf.StringValue;
+import io.spine.core.Command;
+import io.spine.core.Event;
+import io.spine.core.MessageId;
+import io.spine.server.entity.rejection.StandardRejections;
+import io.spine.test.aggregate.ProjectId;
+import io.spine.test.aggregate.command.AggCreateProject;
+import io.spine.testing.client.TestActorRequestFactory;
+import io.spine.testing.server.TestEventFactory;
+import io.spine.type.TypeUrl;
+
+import static io.spine.base.Identifier.newUuid;
+import static io.spine.protobuf.AnyPacker.pack;
+
+public final class CoreMixinsTestEnv {
+
+    /**
+     * Prevents instantiation of this class.
+     */
+    private CoreMixinsTestEnv() {
+    }
+
+    public static MessageId messageId() {
+        Event event = event();
+        return MessageId
+                .newBuilder()
+                .setId(pack(event.getId()))
+                .setTypeUrl(TypeUrl.of(event)
+                                   .value())
+                .setVersion(event.getContext()
+                                 .getVersion())
+                .build();
+    }
+
+    public static Command command() {
+        TestActorRequestFactory factory = new TestActorRequestFactory(CoreMixinsTestEnv.class);
+        ProjectId projectId = ProjectId.newBuilder()
+                                       .setId(newUuid())
+                                       .build();
+        AggCreateProject cmdMessage = AggCreateProject.newBuilder()
+                                                      .setProjectId(projectId)
+                                                      .setName("A Project")
+                                                      .build();
+        return factory.createCommand(cmdMessage);
+    }
+
+    public static Event event() {
+        TestEventFactory factory = TestEventFactory.newInstance(CoreMixinsTestEnv.class);
+        StandardRejections.EntityAlreadyDeleted msg = StandardRejections.EntityAlreadyDeleted
+                .newBuilder()
+                .setEntityId(
+                        pack(StringValue.of("deleted-entity-id")))
+                .build();
+        return factory.createEvent(msg);
+    }
+}
---
+++ server/src/test/java/io/spine/server/dispatch/DispatchMixinsTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.dispatch;
+
+import io.spine.validate.FieldAwareMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Tests of mixins related to the framework message dispatching.
+ */
+class DispatchMixinsTest {
+
+    private static final String SHOULD_MAKE_FIELDS_REACHABLE =
+            "should override `readValue` so that the fields are reachable";
+
+    @Test
+    @DisplayName("`BatchDispatchOutcomeMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void batchDispatchOutcomeMixin() {
+    }
+
+    @Test
+    @DisplayName("`DispatchOutcomeMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void dispatchOutcomeMixin() {
+        checkReachable(DispatchOutcome.getDefaultInstance());
+    }
+
+    @Test
+    @DisplayName("`IgnoreMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void ignoreMixin() {
+        checkReachable(Ignore.getDefaultInstance());
+    }
+
+    @Test
+    @DisplayName("`InterruptionMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void interruptionMixin() {
+        checkReachable(Interruption.getDefaultInstance());
+    }
+
+    @Test
+    @DisplayName("`ProducedCommandsMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void producedCommandsMixin() {
+        checkReachable(ProducedCommands.getDefaultInstance());
+    }
+
+    @Test
+    @DisplayName("`ProducedEventsMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void producedEventsMixin() {
+        checkReachable(ProducedEvents.getDefaultInstance());
+    }
+
+    @Test
+    @DisplayName("`SuccessMixin` " + SHOULD_MAKE_FIELDS_REACHABLE)
+    void successMixin() {
+        checkReachable(Success.getDefaultInstance());
+    }
+
+    private static void checkReachable(FieldAwareMessage msg) {
+        assertThat(msg.checkFieldsReachable()).isTrue();
+    }
+}
