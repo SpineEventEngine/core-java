72f1de6da5773164f6c9db9e2ae955b15a0bf421
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-08-23 21:14:22 +0300

    Fix visibility, remove redundant annotation

--- server/src/main/java/io/spine/server/model/ExternalAttribute.java
+++ server/src/main/java/io/spine/server/model/ExternalAttribute.java
@@ -20,7 +20,6 @@
 package io.spine.server.model;
 
 import com.google.errorprone.annotations.Immutable;
-import io.spine.annotation.Internal;
 import io.spine.core.Subscribe;
 import io.spine.server.command.Command;
 import io.spine.server.event.React;
@@ -38,8 +37,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @see Command#external()
  */
 @Immutable
-@Internal
-public enum ExternalAttribute implements MethodAttribute<Boolean> {
+enum ExternalAttribute implements MethodAttribute<Boolean> {
 
     /** An attribute value for the methods, designed to handle external objects only. */
     EXTERNAL(true),
