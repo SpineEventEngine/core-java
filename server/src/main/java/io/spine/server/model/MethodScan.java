dd188fa14b418f210f678d1a1ab3758528d189c9
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-08-31 19:02:43 +0300

    Suppress PMD warning.

--- server/src/main/java/io/spine/server/model/MethodScan.java
+++ server/src/main/java/io/spine/server/model/MethodScan.java
@@ -116,6 +116,7 @@ final class MethodScan<H extends HandlerMethod<?, ?, ?, ?>> {
         }
     }
 
+    @SuppressWarnings("PMD:CollapsibleIfStatements")    // For clarity.
     private void checkFilteringNotClashes(SelectiveHandler handler) {
         ArgumentFilter filter = handler.filter();
         if (filter.acceptsAll()) {
