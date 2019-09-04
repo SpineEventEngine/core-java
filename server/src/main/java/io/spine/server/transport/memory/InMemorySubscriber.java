6b0bfdd38a22ba6c5ca5799a3412d8d5f8d00553
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-09-04 14:09:19 +0300

    Reduce visibility where `public` is not required

--- server/src/main/java/io/spine/server/transport/memory/InMemorySubscriber.java
+++ server/src/main/java/io/spine/server/transport/memory/InMemorySubscriber.java
@@ -19,7 +19,6 @@
  */
 package io.spine.server.transport.memory;
 
-import io.grpc.Internal;
 import io.spine.server.transport.ChannelId;
 import io.spine.server.transport.Subscriber;
 
@@ -28,10 +27,10 @@ import io.spine.server.transport.Subscriber;
  *
  * <p>To use only in scope of the same JVM as {@linkplain InMemoryPublisher publishers}.
  */
-@Internal
-public class InMemorySubscriber extends Subscriber {
 
-    public InMemorySubscriber(ChannelId id) {
+class InMemorySubscriber extends Subscriber {
+
+    InMemorySubscriber(ChannelId id) {
         super(id);
     }
 }
