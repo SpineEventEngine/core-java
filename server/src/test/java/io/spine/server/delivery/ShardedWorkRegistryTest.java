99b9dfd05abbbe28b9e2c362926ea2ae09578437
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-09-04 18:17:30 +0300

    Add a test for releasing only the required sessions

--- server/src/test/java/io/spine/server/delivery/ShardedWorkRegistryTest.java
+++ server/src/test/java/io/spine/server/delivery/ShardedWorkRegistryTest.java
@@ -22,20 +22,23 @@ package io.spine.server.delivery;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.protobuf.Duration;
 import com.google.protobuf.util.Durations;
 import io.spine.server.NodeId;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth8.assertThat;
 import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterruptibly;
 import static io.spine.server.delivery.given.DeliveryTestEnv.generateNodeId;
 import static io.spine.server.delivery.given.DeliveryTestEnv.newShardIndex;
-import static java.util.stream.IntStream.rangeClosed;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.stream.IntStream.range;
 
 /**
  * An abstract base for {@link ShardedWorkRegistry} tests.
@@ -76,36 +79,56 @@ public abstract class ShardedWorkRegistryTest {
 
         int totalShards = 35;
 
-        ImmutableSet.Builder<ShardIndex> indexBuilder = ImmutableSet.builder();
-        rangeClosed(1, totalShards)
-                .forEach((i) -> {
-                    NodeId newNode = generateNodeId();
-                    ShardIndex newIndex = newShardIndex(i, totalShards);
-
-                    Optional<ShardProcessingSession> session = registry.pickUp(newIndex, newNode);
-                    assertSession(session, newIndex);
-                    indexBuilder.add(newIndex);
-                });
-        ImmutableSet<ShardIndex> indexes = indexBuilder.build();
-
+        ImmutableSet<ShardIndex> indexes = pickUp(registry, totalShards, totalShards);
         Iterable<ShardIndex> releasedIndexes =
                 registry.releaseExpiredSessions(Durations.fromSeconds(100));
         assertThat(releasedIndexes.iterator()
                                   .hasNext()).isFalse();
 
-        sleepUninterruptibly(101, TimeUnit.MILLISECONDS);
+        sleepUninterruptibly(101, MILLISECONDS);
         releasedIndexes = registry.releaseExpiredSessions(Durations.fromMillis(100));
-
-        ImmutableSet<ShardIndex> releasedValues = ImmutableSet.<ShardIndex>builder()
-                .addAll(releasedIndexes)
-                .build();
-        assertThat(releasedValues).isEqualTo(indexes);
+        assertThat(releasedIndexes).containsExactlyElementsIn(indexes);
 
         for (ShardIndex shardIndex : indexes) {
             NodeId anotherNode = generateNodeId();
             Optional<ShardProcessingSession> newSession = registry.pickUp(shardIndex, anotherNode);
             assertSession(newSession, shardIndex);
         }
+    }
+
+    @Test
+    @DisplayName("release a shard only if it's blocked")
+    void testOnlyReleaseBlocked() {
+        ShardedWorkRegistry registry = registry();
+
+        int totalShards = 12;
+
+        Duration expirationPeriod = Durations.fromMillis(1);
+        ImmutableSet<ShardIndex> allIndexes = pickUp(registry, totalShards, totalShards);
+        sleepUninterruptibly(1, SECONDS);
+        registry.releaseExpiredSessions(expirationPeriod);
+
+        // Pick up half of the shards and leave another half empty.
+        ImmutableSet<ShardIndex> newIndexes =
+                pickUp(registry, totalShards, totalShards / 2);
+        sleepUninterruptibly(1, SECONDS);
+        Iterable<ShardIndex> releasedIndexes = registry.releaseExpiredSessions(expirationPeriod);
+        assertThat(releasedIndexes).containsExactlyElementsIn(newIndexes);
+    }
+
+    private static ImmutableSet<ShardIndex>
+    pickUp(ShardedWorkRegistry registry, int outOfTotal, int howMany) {
+        ImmutableSet<ShardIndex> indexes = range(1, howMany)
+                .mapToObj(i -> {
+                    NodeId newNode = generateNodeId();
+                    ShardIndex newIndex = newShardIndex(i, outOfTotal);
+
+                    Optional<ShardProcessingSession> session = registry.pickUp(newIndex, newNode);
+                    assertSession(session, newIndex);
+                    return newIndex;
+                })
+                .collect(toImmutableSet());
+        return indexes;
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")     // asserting the `Optional`.
