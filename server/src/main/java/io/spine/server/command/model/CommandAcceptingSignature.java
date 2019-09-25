6e6a33939f439eb1095013c73d3eb473d3040c57
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-09-25 17:30:35 +0300

    Fix the command-accepting signature behavior.

--- server/src/main/java/io/spine/server/command/model/CommandAcceptingSignature.java
+++ server/src/main/java/io/spine/server/command/model/CommandAcceptingSignature.java
@@ -93,7 +93,7 @@ abstract class CommandAcceptingSignature
             @Override
             public boolean matches(MethodParams params) {
                 return params.are(CommandMessage.class, CommandContext.class)
-                        && params.type(0).isAssignableFrom(GeneratedMessageV3.class);
+                        && GeneratedMessageV3.class.isAssignableFrom(params.type(0));
             }
 
             @Override
