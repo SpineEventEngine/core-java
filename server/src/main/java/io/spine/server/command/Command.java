2c782d8e9bac3d34d3685449fcbbb857b03b4061
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-10-07 18:24:10 +0300

    Document the `@Command`-ing method.

--- server/src/main/java/io/spine/server/command/Command.java
+++ server/src/main/java/io/spine/server/command/Command.java
@@ -31,9 +31,6 @@ import java.lang.annotation.Target;
  * <p>A commanding method <strong>must</strong>:
  * <ul>
  *     <li>be annotated with {@link Command @Command};
- *     <li>return a command message derived from {@link io.spine.base.CommandMessage
- *         CommandMessage}, if there is only one command to be generated;
- *         <strong>or</strong> an {@code Iterable} of command messages for two or more commands;
  *     <li>accept one of the following as the first parameter:
  *     <ul>
  *        <li>a {@linkplain io.spine.base.CommandMessage command message};
@@ -42,41 +39,207 @@ import java.lang.annotation.Target;
  *     </ul>
  * </ul>
  *
- * <p>A commanding method <strong>may</strong> accept a context of the incoming message
- * ({@link io.spine.core.CommandContext CommandContext} or
- * {@link io.spine.core.EventContext EventContext} correspondingly) as the second parameter, if
- * handling of the command requires its context.
+ * <p>Like other message-handling methods, commanding methods are designed to be called by
+ * the framework only. Therefore, it is recommended to declare a them as package-private.
+ * It discourages a developer from calling these methods directly from anywhere.
  *
- * <p>If the annotation is applied to a method which doesn't satisfy any of these requirements,
+ * <p>Package-private access level still declares that a command handler method is a part
+ * of the Bounded Context-level API. See the {@link io.spine.core.BoundedContext
+ * @BoundedContext} description on how the packages and Bounded Contexts relate.
+ *
+ * <h1>Command Transformation</h1>
+ *
+ * <p>Commanding methods may be used to transform an incoming command with one or more commands.
+ * In this case the first method parameter must extend {@linkplain io.spine.base.CommandMessage}.
+ * The returning values must derive from the {@linkplain io.spine.base.CommandMessage} as well.
+ *
+ <p>If a commanding method accepts a command type which is also handled by the application
+ * by another method, a run-time error will occur. This also means that an application cannot have
+ * two command-handling methods that accept the same command type.
+ *
+ * <h2>Accepted Parameters</h2>
+ *
+ * <p>There are several combinations of accepted parameters for a command-transforming method.
+ * The set of available parameters include
+ *
+ * <ul>
+ *
+ * <li>single command message:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  ArchiveTask on(DeleteTask command) { ... }
+ * </pre>
+ *
+ * <li>a command message along with its context:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  MoveTaskToDone on(CompleteTask command, CommandContext context) { ... }
+ * </pre>
+ * </ul>
+ *
+ * <h2>Returning Values</h2>
+ *
+ * <p>A command-transforming method always returns an outcome. Depending on the design intention,
+ * a type of an outcome may vary.
+ *
+ * <p>The command-transforming method must return either
+ *
+ * <ul>
+ *
+ * <li>a command message:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  AssignTask on(StartTask command) { ... }
+ * </pre>
+ *
+ *
+ * <li>{@linkplain io.spine.server.tuple.Either one of} of several command messages:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  {@literal EitherOf2<StopTask, PauseTask>} on(RemoveTaskFromProject command) { ... }
+ * </pre>
+ *
+ *
+ * <li>an {@code Iterable} of command messages:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  {@literal Iterable<PauseTask>} on(PauseProject command) { ... }
+ * </pre>
+ *
+ *
+ * <li>a {@link io.spine.server.tuple.Tuple tuple} of several command messages; being similar
+ * to {@code Iterable}, tuples allow to declare the exact types of returning values, including
+ * {@code Optional} values:
+ *
+ * <pre>
+ *
+ * {@literal @}Command
+ *  {@literal Pair<AssignTask, Optional<StartTask>>} on(AddTaskToProject command) { ... }
+ * </pre>
+ * </ul>
+ *
+ * <h2>Rejecting Commands</h2>
+ *
+ * <p>As a command-handling method, a command-transforming method may reject an incoming command.
+ * In this case, it should declare a generated class derived from
+ * {@link io.spine.base.ThrowableMessage ThrowableMessage} in {@code throws} clause:
+ *
+ * <pre>
+ *
+ * {@literal @}Command
+ *  AssignProject on(CreateProject command) throws CannotCreateProject { ... }
+ * </pre>
+ *
+ * <p>Throwing {@linkplain Throwable other types} of {@code Throwable}s is not allowed in
+ * the command-transforming methods.
+ *
+ * <h1>Command Reaction</h1>
+ *
+ * <p>A commanding method may serve to emit commands in response to an incoming event. In this case
+ * its first parameter must be a message of either {@link io.spine.base.EventMessage EventMessage}
+ * or {@link io.spine.base.RejectionMessage RejectionMessage} type.
+ *
+ * <h2>Accepted Parameters</h2>
+ *
+ * <p>Command-reaction method must accept either
+ *
+ * <ul>
+ *
+ * <li>single event message:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  ScheduleDelivery on(OrderPaid command) { ... }
+ * </pre>
+ *
+ *
+ * <li>an event message along with its context:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  NotifyCustomer on(DeliveryScheduled event, EventContext context) { ... }
+ * </pre>
+ *
+ *
+ * <li>a rejection message:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  CreateProject on(Rejections.CannotCreateProject rejection) { {@literal // change the parameters and try again...} }
+ * </pre>
+ *
+ * <li>a rejection message along with the context of the command which caused it:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  ReassignTask on(Rejections.CannotStartTask rejection, CommandContext) { ... }
+ * </pre>
+ *
+ * </ul>
+ *
+ * <h2>Returning Values</h2>
+ *
+ * <p>A command-reacting method may emit one more more messages deriving from
+ * {@link io.spine.base.CommandMessage CommandMessage}. The possibilities for the return values
+ * are flexible. They serve to describe the returning values as sharp as possible and thus
+ * reduce the probability of a human mistake.
+ *
+ * <p>A command-reacting method must return either
+ * <ul>
+ *
+ * <li>a command message:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  ArchiveTask on(TaskCompleted event) { ... }
+ * </pre>
+ *
+ *
+ * <li>an {@code Optional} command message:
+ * <pre>
+ *
+ * {@literal @}Command
+ * {@literal Optional<StartTask>} on(TaskAdded event) { ... }
+ * </pre>
+ *
+
+ * <li>{@linkplain io.spine.server.tuple.Either one of} of several command messages:
+ * <pre>
+ *
+ * {@literal @}Command
+ *  {@literal EitherOf2<StopTask, PauseTask>} on(TaskReassigned event) { ... }
+ * </pre>
+ *
+ *
+ * <li>an {@code Iterable} of command messages:
+ * <pre>
+ *
+ * {@literal @}Command
+ * {@literal List<ArchiveTask>} on(ProjectCompleted event) { ... }
+ * </pre>
+ *
+ *
+ * <li>a {@link io.spine.server.tuple.Tuple tuple} of several command messages;
+ * it allows to declare the exact types of returning values, including {@code Optional}s:
+ *
+ * <pre>
+ *
+ * {@literal @}Command
+ *  {@literal Triplet<AssignTask, UpdateTaskDueDate, Optional<StartTask>>} on(TaskCreated command) { ... }
+ * </pre>
+ * </ul>
+ *
+ * <p>If the annotation is applied to a method which doesn't satisfy either of these requirements,
  * this method is not considered a command handler and is <strong>not</strong> registered for
  * command generation.
  *
- * <h1>Throwables</h1>
- * <p>A commanding method may throw a {@link io.spine.base.ThrowableMessage ThrowableMessage}
- * <em>if, and only if</em> it accepts a command message. This means that the incoming command
- * may be either rejected, or substituting command(s) must be generated.
- *
- * <p>Throwing {@linkplain Throwable other types} in command transforming methods is not allowed.
- *
- * <p>Commanding methods accepting events or rejections may not throw.
- *
- * <h2>Access Modifiers</h2>
- *
- * <p>A commanding method <strong>should</strong> have package-private access. It will allow
- * calling this method from tests. The method should not be {@code public} because it is not
- * supposed to be called directly.
- *
- * <h2>Handle or Transform a Command</h2>
- *
- * <p>An application can either {@linkplain io.spine.server.command.Assign handle} an incoming
- * command <strong>or</strong> transform it into one or more commands that will be handled instead.
- * Because of this, a commanding method cannot accept a command message, if there is already a
- * command handling method which accepts the command message type.
- *
- * <p>If a commanding method accepts a command type which is also handled by the application
- * by another method, a run-time error will occur. This also means that an application cannot have
- * two commanding methods that accept the same command type.
- *
+ * @see io.spine.server.tuple.Tuple Returning Two or More Command Messages
+ * @see io.spine.server.tuple.Either Returning One of Command Messages
  * @see io.spine.server.command.Assign Handling Commands
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -87,7 +250,7 @@ public @interface Command {
      * When {@code true}, the annotated method receives an event generated from outside of the
      * Bounded Context to which the annotated method's class belongs.
      *
-     * <p>Should only be applied to commander methods that accept an event or rejection as a
+     * <p>Should only be applied to commanding methods that accept an event or rejection as a
      * parameter.
      *
      * <p>If applied to a command receiving method, the Model
