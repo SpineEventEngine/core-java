ff49863b36e433dea0f1cbdd1c57892f91716bc6
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-10-31 22:02:04 +0200

    Introduce timeout value

---
+++ client/src/main/java/io/spine/client/Timeout.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.client;
+
+import com.google.common.base.MoreObjects;
+import com.google.errorprone.annotations.Immutable;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A timout value with unit.
+ *
+ * @implNote This class is created instead of reusing {@code okio.Timeout} to avoid
+ *         dependency on the library which is a transitive dependency of gRPC.
+ *         Also, {@code okio.Timeout} does a lot more than we need in this package.
+ */
+@Immutable
+final class Timeout {
+
+    private final long value;
+    private final TimeUnit unit;
+
+    static Timeout of(long value, TimeUnit unit) {
+        checkNotNull(unit);
+        return new Timeout(value, unit);
+    }
+
+    private Timeout(long value, TimeUnit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    long value() {
+        return value;
+    }
+
+    TimeUnit unit() {
+        return unit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Timeout timeout = (Timeout) o;
+        return value == timeout.value &&
+                unit == timeout.unit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, unit);
+    }
+
+    @Override
+    @SuppressWarnings("DuplicateStringLiteralInspection") // local field name semantics
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                          .add("value", value)
+                          .add("unit", unit)
+                          .toString();
+    }
+}
