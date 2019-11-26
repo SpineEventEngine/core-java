f86a3d96da87c117fd84382af5dc286f2419b458
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-11-26 18:27:52 +0200

    Extract and document some classes from `DeliveryTest`.

---
+++ server/src/test/java/io/spine/server/delivery/InboxContents.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.delivery;
+
+import com.google.common.collect.ImmutableMap;
+import io.spine.core.TenantId;
+import io.spine.server.ServerEnvironment;
+
+import static io.spine.server.tenant.TenantAwareRunner.with;
+
+/**
+ * Utility providing an access to the raw contents of the {@link Inbox}es in the current
+ * {@linkplain io.spine.server.ServerEnvironment server environment}.
+ */
+final class InboxContents {
+
+    private InboxContents() {
+    }
+
+    /**
+     * Fetches the contents of the {@code Inbox}es for each of the {@code ShardIndex}es.
+     */
+    static ImmutableMap<ShardIndex, Page<InboxMessage>> get() {
+        Delivery delivery = ServerEnvironment.instance()
+                                             .delivery();
+        InboxStorage storage = delivery.storage();
+        int shardCount = delivery.shardCount();
+        ImmutableMap.Builder<ShardIndex, Page<InboxMessage>> builder =
+                ImmutableMap.builder();
+        for (int shardIndex = 0; shardIndex < shardCount; shardIndex++) {
+            ShardIndex index =
+                    ShardIndex.newBuilder()
+                              .setIndex(shardIndex)
+                              .setOfTotal(shardCount)
+                              .vBuild();
+            Page<InboxMessage> page = with(TenantId.getDefaultInstance())
+                    .evaluate(() -> storage.readAll(index, Integer.MAX_VALUE));
+
+            builder.put(index, page);
+        }
+
+        return builder.build();
+    }
+}
---
+++ server/src/test/java/io/spine/server/delivery/NastyClient.java
@@ -0,0 +1,295 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.delivery;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterators;
+import io.spine.server.ServerEnvironment;
+import io.spine.server.delivery.given.CalcAggregate;
+import io.spine.server.delivery.given.CalculatorSignal;
+import io.spine.server.delivery.given.DeliveryTestEnv;
+import io.spine.test.delivery.AddNumber;
+import io.spine.test.delivery.Calc;
+import io.spine.test.delivery.NumberImported;
+import io.spine.test.delivery.NumberReacted;
+import io.spine.testing.server.blackbox.BlackBoxBoundedContext;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.Streams.concat;
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Posts numerous messages to instances of {@link CalcAggregate} in a selected number of threads
+ * and verifies that each of the targets calculated a proper sum.
+ *
+ * <p>Verifies the results of its job by inspecting the system environment and repositories.
+ */
+class NastyClient {
+
+    private final int threadCount;
+    private final boolean shouldInboxBeEmpty;
+    private final DeliveryTestEnv.CalculatorRepository repository;
+
+    /** Which signals are expected to be delivered to which targets. */
+    private @Nullable Map<String, List<CalculatorSignal>> signalsPerTarget;
+
+    /**
+     * Creates the client operating simultaneously in a specified number of threads.
+     *
+     * <p>Also, the client is going to assume that all inboxes should be empty, once all
+     * the messages posted by the client are propagated.
+     */
+    NastyClient(int threadCount) {
+        this(threadCount, true);
+    }
+
+    /**
+     * Create the client operating in several number of threads.
+     *
+     * <p>This constructor allows to tell weather the system inboxes should be empty after
+     * all the messages posted by the client are propagated.
+     */
+    NastyClient(int threadCount, boolean shouldInboxBeEmpty) {
+        this.threadCount = threadCount;
+        this.shouldInboxBeEmpty = shouldInboxBeEmpty;
+        this.repository = new DeliveryTestEnv.CalculatorRepository();
+    }
+
+    /**
+     * Generates some number of commands and events and delivers them to the specified
+     * {@linkplain CalcAggregate target entities} via the selected number of threads.
+     *
+     * @param targets
+     *         the identifiers of target entities
+     */
+    void runWith(Set<String> targets) {
+        BlackBoxBoundedContext<?> context =
+                BlackBoxBoundedContext.singleTenant()
+                                      .with(repository);
+
+        DeliveryTestEnv.SignalMemoizer memoizer = subscribeToDelivered();
+
+        int streamSize = targets.size() * 30;
+
+        Iterator<String> targetsIterator = Iterators.cycle(targets);
+        List<AddNumber> commands = commands(streamSize, targetsIterator);
+        List<NumberImported> importEvents = eventsToImport(streamSize, targetsIterator);
+        List<NumberReacted> reactEvents = eventsToReact(streamSize, targetsIterator);
+
+        postAsync(context, commands, importEvents, reactEvents);
+
+        Stream<CalculatorSignal> signals = concat(commands.stream(),
+                                                  importEvents.stream(),
+                                                  reactEvents.stream());
+
+        signalsPerTarget = signals.collect(groupingBy(CalculatorSignal::getCalculatorId));
+
+        for (String calcId : signalsPerTarget.keySet()) {
+
+            ImmutableSet<CalculatorSignal> receivedMessages = memoizer.messagesBy(calcId);
+            Set<CalculatorSignal> targetSignals =
+                    ImmutableSet.copyOf(signalsPerTarget.get(calcId));
+            assertEquals(targetSignals, receivedMessages);
+
+            Integer sumForTarget = targetSignals.stream()
+                                                .map(CalculatorSignal::getValue)
+                                                .reduce(0, Integer::sum);
+            Calc expectedState = Calc.newBuilder()
+                                     .setId(calcId)
+                                     .setSum(sumForTarget)
+                                     .build();
+            context.assertEntity(CalcAggregate.class, calcId)
+                   .hasStateThat()
+                   .comparingExpectedFieldsOnly()
+                   .isEqualTo(expectedState);
+
+        }
+        ensureInboxesEmpty();
+    }
+
+    /**
+     * Returns the number of calls to {@link #doStore(CalcAggregate)} method there were.
+     *
+     * @param id
+     *         identifier of the {@link CalcAggregate}, calls to which are counted
+     */
+    int callsToRepoStore(String id) {
+        return repository.storeCallsCount(id);
+    }
+
+
+    /**
+     * Returns the number of calls to  {@link #doLoadOrCreate(String)} method there were.
+     *
+     * @param id
+     *         identifier of the {@link CalcAggregate}, calls to which are counted
+     */
+    int callsToRepoLoadOrCreate(String id) {
+        return repository.loadOrCreateCallsCount(id);
+    }
+
+    /**
+     * Returns the collection of signals per target, which are expected to be delivered.
+     */
+    ImmutableMap<String, List<CalculatorSignal>> signalsPerTarget() {
+        if (signalsPerTarget == null) {
+            return ImmutableMap.of();
+        }
+        return ImmutableMap.copyOf(signalsPerTarget);
+    }
+
+    private static List<NumberReacted> eventsToReact(int streamSize,
+                                                     Iterator<String> targetsIterator) {
+        IntStream ints = IntStream.range(0, streamSize);
+        return ints.mapToObj((value) ->
+                                     NumberReacted.newBuilder()
+                                                  .setCalculatorId(targetsIterator.next())
+                                                  .setValue(value)
+                                                  .vBuild())
+                   .collect(toList());
+    }
+
+    private static List<NumberImported> eventsToImport(int streamSize,
+                                                       Iterator<String> targetsIterator) {
+        IntStream ints = IntStream.range(streamSize, streamSize * 2);
+        return ints.mapToObj((value) ->
+                                     NumberImported.newBuilder()
+                                                   .setCalculatorId(targetsIterator.next())
+                                                   .setValue(value)
+                                                   .vBuild())
+                   .collect(toList());
+    }
+
+    private static List<AddNumber> commands(int streamSize, Iterator<String> targetsIterator) {
+        IntStream ints = IntStream.range(streamSize * 2, streamSize * 3);
+        return ints.mapToObj((value) ->
+                                     AddNumber.newBuilder()
+                                              .setCalculatorId(targetsIterator.next())
+                                              .setValue(value)
+                                              .vBuild())
+                   .collect(toList());
+    }
+
+    private static DeliveryTestEnv.SignalMemoizer subscribeToDelivered() {
+        DeliveryTestEnv.SignalMemoizer observer = new DeliveryTestEnv.SignalMemoizer();
+        ServerEnvironment.instance()
+                         .delivery()
+                         .subscribe(observer);
+        return observer;
+    }
+
+    private void ensureInboxesEmpty() {
+        if (shouldInboxBeEmpty) {
+            ImmutableMap<ShardIndex, Page<InboxMessage>> shardedItems = InboxContents.get();
+
+            for (ShardIndex index : shardedItems.keySet()) {
+                Page<InboxMessage> page = shardedItems.get(index);
+                assertTrue(page.contents()
+                               .isEmpty());
+                assertFalse(page.next()
+                                .isPresent());
+            }
+        }
+    }
+
+    private void postAsync(BlackBoxBoundedContext context,
+                           List<AddNumber> commands,
+                           List<NumberImported> eventsToImport,
+                           List<NumberReacted> eventsToReact) {
+
+        Stream<Callable<Object>> signalStream =
+                concat(
+                        commandCallables(context, commands),
+                        importEventCallables(context, eventsToImport),
+                        reactEventsCallables(context, eventsToReact)
+                );
+        Collection<Callable<Object>> signals = signalStream.collect(toList());
+        if (1 == threadCount) {
+            runSync(signals);
+        } else {
+            runAsync(signals);
+        }
+    }
+
+    private void runAsync(Collection<Callable<Object>> signals) {
+        ExecutorService executorService = newFixedThreadPool(threadCount);
+        try {
+            executorService.invokeAll(signals);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdownNow();
+        }
+    }
+
+    private static void runSync(Collection<Callable<Object>> signals) {
+        for (Callable<Object> signal : signals) {
+            try {
+                signal.call();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static Stream<Callable<Object>> commandCallables(BlackBoxBoundedContext context,
+                                                             List<AddNumber> commands) {
+        return commands.stream()
+                       .map((c) -> () -> {
+                           context.receivesCommand(c);
+                           return new Object();
+                       });
+    }
+
+    private static Stream<Callable<Object>> importEventCallables(BlackBoxBoundedContext context,
+                                                                 List<NumberImported> events) {
+        return events.stream()
+                     .map((e) -> () -> {
+                         context.importsEvent(e);
+                         return new Object();
+                     });
+    }
+
+    private static Stream<Callable<Object>> reactEventsCallables(BlackBoxBoundedContext context,
+                                                                 List<NumberReacted> events) {
+        return events.stream()
+                     .map((e) -> () -> {
+                         context.receivesEvent(e);
+                         return new Object();
+                     });
+    }
+}
