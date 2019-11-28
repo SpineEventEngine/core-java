4fd2272a771279ea79114e2397e75748b89e0cbc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-11-28 21:50:12 +0200

    Encapsulate filtering field checks

--- client/src/main/java/io/spine/client/FilterMixin.java
+++ client/src/main/java/io/spine/client/FilterMixin.java
@@ -21,117 +21,20 @@
 package io.spine.client;
 
 import com.google.protobuf.Any;
-import com.google.protobuf.Descriptors.Descriptor;
-import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Message;
 import io.spine.annotation.GeneratedMixin;
-import io.spine.base.EntityState;
 import io.spine.base.Field;
-import io.spine.base.FieldPath;
-import io.spine.code.proto.FieldDeclaration;
 import io.spine.protobuf.TypeConverter;
-import io.spine.type.TypeUrl;
-
-import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static io.spine.client.OperatorEvaluator.eval;
-import static io.spine.code.proto.ColumnOption.isColumn;
 import static io.spine.util.Exceptions.newIllegalArgumentException;
-import static io.spine.util.Exceptions.newIllegalStateException;
 
 /**
  * Augments {@link Filter} with useful methods.
  */
 @GeneratedMixin
 interface FilterMixin extends FilterOrBuilder, MessageFilter<Message> {
-
-    /**
-     * Obtains the target field.
-     */
-    default Field field() {
-        FieldPath fieldPath = getFieldPath();
-        return Field.withPath(fieldPath);
-    }
-
-    /**
-     * Checks if the target field is present in the specified message type.
-     */
-    default boolean fieldPresentIn(Descriptor message) {
-        checkNotNull(message);
-        Field field = field();
-        boolean result = field.presentIn(message);
-        return result;
-    }
-
-    /**
-     * Verifies that the target field is present in the passed message type.
-     *
-     * @throws IllegalArgumentException
-     *         if the field is not present in the passed message type
-     */
-    default void checkFieldPresentIn(Descriptor message) {
-        checkNotNull(message);
-        if (!fieldPresentIn(message)) {
-            throw newIllegalStateException(
-                    "The field with path `%s` is not present in the message type `%s`.",
-                    field(), message.getFullName());
-        }
-    }
-
-    /**
-     * Checks if the target field is an entity column in the passed message type.
-     */
-    default boolean fieldIsColumnIn(Descriptor message) {
-        checkNotNull(message);
-        Optional<FieldDescriptor> fieldDescriptor = field().findDescriptor(message);
-        if (!fieldDescriptor.isPresent()) {
-            return false;
-        }
-        FieldDeclaration declaration = new FieldDeclaration(fieldDescriptor.get());
-        boolean result = isColumn(declaration);
-        return result;
-    }
-
-    /**
-     * Verifies that the target field is an entity column in the passed message type.
-     *
-     * @throws IllegalArgumentException
-     *         if the field is not an entity column in the passed message type
-     */
-    default void checkFieldIsColumnIn(Descriptor message) {
-        checkNotNull(message);
-        checkFieldAtTopLevel();
-        if (!fieldIsColumnIn(message)) {
-            throw newIllegalStateException(
-                    "The column `%s` is not found in entity state type `%s`. " +
-                         "Please check the field exists and is marked with the `(column)` option.",
-                    field(), message.getFullName());
-        }
-    }
-
-    /**
-     * Checks if the target field is a top-level field.
-     */
-    default boolean fieldAtTopLevel() {
-        return !field().isNested();
-    }
-
-    /**
-     * Verifies that the target field is a top-level field.
-     *
-     * @throws IllegalArgumentException
-     *         if the field is not a top-level field
-     */
-    default void checkFieldAtTopLevel() {
-        if (!fieldAtTopLevel()) {
-            throw newIllegalStateException(
-                    "The entity filter contains a nested entity column `%s`. " +
-                            "Nested entity columns are currently not supported.",
-                    field()
-            );
-        }
-    }
 
     /**
      * Verifies that the filter can be applied to the given {@code target}.
@@ -144,16 +47,8 @@ interface FilterMixin extends FilterOrBuilder, MessageFilter<Message> {
      */
     default void checkCanApplyTo(Target target) {
         checkNotNull(target);
-        TypeUrl targetType = target.typeUrl();
-
-        Class<Message> javaClass = targetType.getMessageClass();
-        Descriptor descriptor = targetType.toTypeName()
-                                          .messageDescriptor();
-        if (EntityState.class.isAssignableFrom(javaClass)) {
-            checkFieldIsColumnIn(descriptor);
-        } else {
-            checkFieldPresentIn(descriptor);
-        }
+        FilteringField field = new FilteringField(this);
+        field.checkAppliesTo(target);
     }
 
     /**
@@ -161,7 +56,7 @@ interface FilterMixin extends FilterOrBuilder, MessageFilter<Message> {
      */
     @Override
     default boolean test(Message message) {
-        Field field = field();
+        Field field = Field.withPath(getFieldPath());
         Object actual = field.valueIn(message);
         Any requiredAsAny = getValue();
         Object required = TypeConverter.toObject(requiredAsAny, actual.getClass());
