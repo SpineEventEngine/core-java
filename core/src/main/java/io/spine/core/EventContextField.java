60187442c8ef02768900cef746c2f7a6fcdefc08
Dima Kuzmin <kuzmindima21@gmail.com>
2020-02-08 15:16:00 +0200

    Add doc to `EventContextField`

--- core/src/main/java/io/spine/core/EventContextField.java
+++ core/src/main/java/io/spine/core/EventContextField.java
@@ -3,6 +3,13 @@ package io.spine.core;
 import io.spine.base.Field;
 import io.spine.base.SubscribableField;
 
+/**
+ * A subscribable field of an event context.
+ *
+ * <p>When such field is specified to a subscription filter on creation, the {@code "context."}
+ * prefix will be automatically appended to the field path, allowing to distinguish between event
+ * context and event message filters when sending the request to a server side.
+ */
 public class EventContextField extends SubscribableField {
 
     public EventContextField(Field field) {
