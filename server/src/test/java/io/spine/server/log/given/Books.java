08745a6e4f52d44ed58ee73699a96fd942d59d86
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-02-28 15:45:07 +0200

    Add a test env for the handler log tests

---
+++ server/src/test/java/io/spine/server/log/given/Books.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.log.given;
+
+import io.spine.people.PersonName;
+import io.spine.server.log.Book;
+import io.spine.server.log.Isbn;
+
+public final class Books {
+
+    public static final Isbn BIG_BLUE_BOOK = isbn("978-0321125217");
+    public static final Isbn BIG_RED_BOOK = isbn("978-0321834577");
+    public static final Isbn SMALL_GREEN_BOOK = isbn("978-0134434421");
+
+    private static final String PUBLISHER = "A-W";
+
+    /**
+     * Prevents the utility class instantiation.
+     */
+    private Books() {
+    }
+
+    public static Book domainDrivenDesign() {
+        return Book
+                .newBuilder()
+                .setIsbn(BIG_BLUE_BOOK)
+                .setTitle("Domain-Driven Design")
+                .addAuthor(PersonName.newBuilder().setGivenName("Eric"))
+                .setPublisher(PUBLISHER)
+                .vBuild();
+    }
+
+    public static Book implementingDdd() {
+        return Book
+                .newBuilder()
+                .setIsbn(BIG_BLUE_BOOK)
+                .setTitle("Implementing DDD")
+                .addAuthor(PersonName.newBuilder().setGivenName("Vaughn"))
+                .setPublisher(PUBLISHER)
+                .vBuild();
+    }
+
+    public static Book dddDistilled() {
+        return Book
+                .newBuilder()
+                .setIsbn(BIG_BLUE_BOOK)
+                .setTitle("DDD Distilled")
+                .addAuthor(PersonName.newBuilder().setGivenName("Vaughn"))
+                .setPublisher(PUBLISHER)
+                .vBuild();
+    }
+
+    private static Isbn isbn(String value) {
+        return Isbn
+                .newBuilder()
+                .setValue(value)
+                .build();
+    }
+}
---
+++ server/src/test/java/io/spine/server/log/given/CardAggregate.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.log.given;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.spine.people.PersonName;
+import io.spine.server.aggregate.Aggregate;
+import io.spine.server.aggregate.Apply;
+import io.spine.server.command.Assign;
+import io.spine.server.log.Book;
+import io.spine.server.log.BookReturned;
+import io.spine.server.log.BooksBorrowed;
+import io.spine.server.log.BorrowBooks;
+import io.spine.server.log.Isbn;
+import io.spine.server.log.LibraryCard;
+import io.spine.server.log.LibraryCardId;
+import io.spine.server.log.ReturnBook;
+import io.spine.server.log.UnknownBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.spine.server.log.given.Books.BIG_BLUE_BOOK;
+import static io.spine.server.log.given.Books.BIG_RED_BOOK;
+import static io.spine.server.log.given.Books.SMALL_GREEN_BOOK;
+import static io.spine.server.log.given.Books.dddDistilled;
+import static io.spine.server.log.given.Books.domainDrivenDesign;
+import static io.spine.server.log.given.Books.implementingDdd;
+import static java.util.logging.Level.FINE;
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Level.WARNING;
+
+public final class CardAggregate
+        extends Aggregate<LibraryCardId, LibraryCard, LibraryCard.Builder> {
+
+    private static final ImmutableMap<Isbn, Book> knownBooks = ImmutableMap.of(
+            BIG_BLUE_BOOK, domainDrivenDesign(),
+            BIG_RED_BOOK, implementingDdd(),
+            SMALL_GREEN_BOOK, dddDistilled()
+    );
+
+    @Assign
+    BooksBorrowed handle(BorrowBooks command) throws UnknownBook {
+        BooksBorrowed.Builder event = BooksBorrowed
+                .newBuilder()
+                .setCard(id());
+        List<Isbn> unknownBooks = new ArrayList<>();
+        for (Isbn bookId : command.getBookIdList()) {
+            Book book = knownBooks.get(bookId);
+            if (book != null) {
+                event.addBook(book);
+                List<PersonName> authors = book.getAuthorList();
+                PersonName firstAuthor = authors.get(0);
+                log().at(FINE)
+                     .log("Adding to order: %s by %s %s",
+                          book.getTitle(), firstAuthor.getGivenName(), firstAuthor.getFamilyName());
+            } else {
+                log().at(WARNING)
+                     .log("Cannot lend an unknown book. ISBN: `%s`", bookId.getValue());
+                unknownBooks.add(bookId);
+            }
+        }
+        if (!unknownBooks.isEmpty()) {
+            throw UnknownBook
+                    .newBuilder()
+                    .addAllBook(unknownBooks)
+                    .build();
+        } else {
+            return event.vBuild();
+        }
+    }
+
+    @Assign
+    BookReturned handle(ReturnBook command) throws UnknownBook {
+        Isbn isbn = command.getBook();
+        Book book = knownBooks.get(isbn);
+        if (book != null) {
+            UnknownBook rejection = UnknownBook
+                    .newBuilder()
+                    .addAllBook(ImmutableList.of(isbn))
+                    .build();
+            log().at(SEVERE)
+                 .withCause(rejection)
+                 .log("Cannot return an unknown book. ISBN: `%s`", isbn.getValue());
+            throw rejection;
+        } else {
+            return BookReturned
+                    .newBuilder()
+                    .setCard(id())
+                    .setBook(book)
+                    .vBuild();
+        }
+    }
+
+    @Apply
+    private void on(BooksBorrowed event) {
+        builder().addAllBook(event.getBookList());
+    }
+
+    @Apply
+    private void on(BookReturned event) {
+        List<Book> list = builder().getBookList();
+        Book book = event.getBook();
+        int bookIndex = list.indexOf(book);
+        builder().removeBook(bookIndex);
+    }
+}
