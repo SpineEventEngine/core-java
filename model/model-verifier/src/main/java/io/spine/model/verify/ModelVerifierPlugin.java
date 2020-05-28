44e8e9f18316415151919993042f417ae906bb79
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-05-28 19:56:02 +0300

    Remove task input properties

--- model/model-verifier/src/main/java/io/spine/model/verify/ModelVerifierPlugin.java
+++ model/model-verifier/src/main/java/io/spine/model/verify/ModelVerifierPlugin.java
@@ -69,7 +69,6 @@ public final class ModelVerifierPlugin extends SpinePlugin {
         newTask(verifyModel, action(rawModelStorage))
                 .insertBeforeTask(classes)
                 .insertAfterTask(compileJava)
-                .withInputFiles(project.fileTree(rawModelStorage))
                 .applyNowTo(project);
     }
 
