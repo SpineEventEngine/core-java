71106cba12d7d342bd07ee4290c8324d05ef8284
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2020-08-07 22:36:45 +0300

    Advance version

--- version.gradle.kts
+++ version.gradle.kts
@@ -27,4 +27,4 @@
 
 val spineBaseVersion: String by extra("1.5.21")
 val spineTimeVersion: String by extra("1.5.21")
-val versionToPublish: String by extra("1.5.21")
+val versionToPublish: String by extra("1.5.22")
