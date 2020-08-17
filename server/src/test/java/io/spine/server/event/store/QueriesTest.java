728f95733789642217be6e141889bad13a807e59
Alex Tymchenko <alex.tymchenko@teamdev.com>
2020-08-17 23:30:20 +0300

    Test `Queries` for `null` intolerance of the params and ensure it has a private parameterless ctor. Rephrase some of the display names.

--- server/src/test/java/io/spine/server/event/store/QueriesTest.java
+++ server/src/test/java/io/spine/server/event/store/QueriesTest.java
@@ -21,6 +21,7 @@
 package io.spine.server.event.store;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.testing.NullPointerTester;
 import com.google.protobuf.util.Timestamps;
 import io.spine.core.Event;
 import io.spine.core.EventId;
@@ -33,27 +34,45 @@ import io.spine.server.event.EventStreamQuery;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.google.common.testing.NullPointerTester.Visibility.PACKAGE;
 import static com.google.common.truth.Truth.assertThat;
 import static io.spine.query.LogicalOperator.AND;
 import static io.spine.query.LogicalOperator.OR;
 import static io.spine.server.event.store.Queries.convert;
+import static io.spine.testing.DisplayNames.HAVE_PARAMETERLESS_CTOR;
+import static io.spine.testing.DisplayNames.NOT_ACCEPT_NULLS;
+import static io.spine.testing.Tests.assertHasPrivateParameterlessCtor;
 
 @DisplayName("`Queries` should")
 class QueriesTest {
 
     @Test
-    @DisplayName("convert empty query to an empty `RecordQuery`")
+    @DisplayName(HAVE_PARAMETERLESS_CTOR)
+    void haveUtilityConstructor() {
+        assertHasPrivateParameterlessCtor(Queries.class);
+    }
+
+    @Test
+    @DisplayName(NOT_ACCEPT_NULLS)
+    void passNullToleranceCheck() {
+        new NullPointerTester()
+                .testStaticMethods(Queries.class, PACKAGE);
+    }
+
+    @Test
+    @DisplayName("convert an empty query to an empty `RecordQuery`")
     void convertEmptyToFilters() {
         EventStreamQuery query = EventStreamQuery.newBuilder()
                                                  .build();
         RecordQuery<EventId, Event> result = convert(query);
         Subject<EventId, Event> subject = result.subject();
-        assertThat(subject.id().values()).isEmpty();
+        assertThat(subject.id()
+                          .values()).isEmpty();
         assertThat(subject.predicates()).isEmpty();
     }
 
     @Test
-    @DisplayName("convert time query to the corresponding `RecordQuery`")
+    @DisplayName("convert the time-constrained query to the corresponding `RecordQuery`")
     void convertTimeToFilters() {
         EventStreamQuery query = EventStreamQuery
                 .newBuilder()
@@ -71,7 +90,7 @@ class QueriesTest {
     }
 
     @Test
-    @DisplayName("convert type query to the corresponding `RecordQuery`")
+    @DisplayName("convert the event-type query to the corresponding `RecordQuery`")
     void convertTypeToFilters() {
         String expected = " com.example.EventType ";
         EventFilter validFilter = filterForType(expected);
