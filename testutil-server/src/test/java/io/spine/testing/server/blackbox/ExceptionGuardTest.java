c08749bca618472be2e8b7f6524e5446391a64fa
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-11-18 11:03:29 +0200

    Introduce ExceptionGuard test

---
+++ testutil-server/src/test/java/io/spine/testing/server/blackbox/ExceptionGuardTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.testing.server.blackbox;
+
+import io.spine.base.Error;
+import io.spine.system.server.HandlerFailedUnexpectedly;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+
+import java.util.logging.Logger;
+
+import static com.google.common.truth.Truth.assertThat;
+import static io.spine.base.Errors.causeOf;
+
+@DisplayName("`ExceptionGuard` should")
+final class ExceptionGuardTest extends ExceptionLoggingTest {
+
+    private ExceptionGuard guard;
+
+    @BeforeEach
+    void initGuard() {
+        guard = new ExceptionGuard();
+    }
+
+    @Test
+    @DisplayName("log `HandlerFailedUnexpectedly` event")
+    void tolerateException() {
+        guard.tolerate();
+        Error error = causeOf(new IllegalStateException("Test exception. Handler is fine."));
+        guard.on(
+                HandlerFailedUnexpectedly
+                        .newBuilder()
+                        .setEntity(entity())
+                        .setError(error)
+                        .vBuild()
+        );
+        assertLogged(error.getMessage());
+    }
+
+    @Test
+    @DisplayName("fail test on `HandlerFailedUnexpectedly` event")
+    @SuppressWarnings({"ThrowCaughtLocally", "ErrorNotRethrown"})
+        // we're testing that guard is failing a test
+    void failTest() {
+        Error error = causeOf(new IllegalStateException("Test exception. Handler is fine."));
+        try {
+            guard.on(
+                    HandlerFailedUnexpectedly
+                            .newBuilder()
+                            .setEntity(entity())
+                            .setError(error)
+                            .vBuild()
+            );
+            throw new IllegalStateException("The guard must have failed the test.");
+        } catch (AssertionFailedError e) {
+            assertThat(e.getMessage()).contains(error.getMessage());
+        } catch (IllegalStateException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Override
+    protected Logger logger() {
+        return Logger.getLogger(ExceptionGuard.class.getName());
+    }
+}
