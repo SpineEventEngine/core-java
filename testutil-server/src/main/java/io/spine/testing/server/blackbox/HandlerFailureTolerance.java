253c36eff6276230362ed461f918c8d38f1a50ba
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-11-19 09:08:31 +0200

    Do not expose the enum.

--- testutil-server/src/main/java/io/spine/testing/server/blackbox/HandlerFailureTolerance.java
+++ testutil-server/src/main/java/io/spine/testing/server/blackbox/HandlerFailureTolerance.java
@@ -24,7 +24,7 @@ package io.spine.testing.server.blackbox;
  * Configures the behavior of the {@link BlackBoxContext} on handling runtime exceptions
  * thrown from the handler methods.
  */
-public enum HandlerFailureTolerance {
+enum HandlerFailureTolerance {
 
     /**
      * Identifies that exceptions must be only logged.
