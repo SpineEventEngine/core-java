2c064c59113359de1872720e100e0c3f7687ddc9
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2020-12-18 20:27:38 +0200

    Add `update` block extension function

---
+++ server/src/test/kotlin/io/spine/server/entity/ExtensionsTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package io.spine.server.entity
+
+import com.google.common.truth.Truth.assertThat
+import io.spine.server.test.shared.StringEntity
+import io.spine.testing.TestValues.randomString
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("Kotlin extensions of `TransactionalEntity` should")
+internal class ExtensionsTest {
+
+    @Test
+    fun `add 'update' block handler for passing properties to 'builder'`() {
+        val entity = createEntity()
+        val str = randomString()
+        entity.apply(str)
+        assertThat(entity.value())
+            .isEqualTo(str)
+    }
+}
+
+/**
+ * Creates the test fixture entity and injects a stub transaction so
+ * that [TransactionalEntity.builder] is available.
+ */
+private fun createEntity() : Fixture {
+    val entity = Fixture()
+    val tx: Transaction<String, Fixture, StringEntity, StringEntity.Builder> =
+        StubTransaction(entity, true, true)
+    entity.injectTransaction(tx)
+    return entity
+}
+
+/**
+ * An entity which uses the `update` extension in its [apply] method.
+ */
+private class Fixture : TransactionalEntity<String, StringEntity, StringEntity.Builder>() {
+
+    init {
+        setId(randomString())
+    }
+
+    fun apply(s: String) {
+        update {
+            value = s
+        }
+        setState(builder().vBuild())
+    }
+
+    fun value(): String = state().value
+}
