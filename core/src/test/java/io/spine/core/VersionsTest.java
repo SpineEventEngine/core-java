9a510152001b17553f5ea55081b00eea3f31141f
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2020-12-25 14:46:08 +0200

    Simplify tests

--- core/src/test/java/io/spine/core/VersionsTest.java
+++ core/src/test/java/io/spine/core/VersionsTest.java
@@ -1,6 +1,12 @@
 /*
  * Copyright 2020, TeamDev. All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
  * disclaimer.
@@ -21,23 +27,20 @@
 package io.spine.core;
 
 import com.google.common.testing.NullPointerTester;
+import io.spine.testing.UtilityClassTest;
 import io.spine.testing.core.given.GivenVersion;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.spine.testing.DisplayNames.HAVE_PARAMETERLESS_CTOR;
 import static io.spine.testing.DisplayNames.NOT_ACCEPT_NULLS;
-import static io.spine.testing.Tests.assertHasPrivateParameterlessCtor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("Versions utility should")
-class VersionsTest {
+@DisplayName("`Versions` utility should")
+class VersionsTest extends UtilityClassTest<Versions> {
 
-    @Test
-    @DisplayName(HAVE_PARAMETERLESS_CTOR)
-    void haveUtilityConstructor() {
-        assertHasPrivateParameterlessCtor(Versions.class);
+    private VersionsTest() {
+        super(Versions.class);
     }
 
     @Test
