b773765192f6f6a132739819bc40d951b243a1dc
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2020-12-25 14:21:19 +0200

    Bump base

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,6 +1,12 @@
 /*
  * Copyright 2020, TeamDev. All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
  * disclaimer.
@@ -39,7 +45,7 @@ val coreJava = "2.0.0-jdk8.SNAPSHOT.9"
 /**
  * Versions of the Spine libraries that `core-java` depends on.
  */
-val base = "2.0.0-jdk8.SNAPSHOT.8"
+val base = "2.0.0-jdk8.SNAPSHOT.10"
 val time = "2.0.0-jdk8.SNAPSHOT.8"
 
 project.extra.apply {
