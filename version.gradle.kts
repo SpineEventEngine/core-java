456b8a8dd66865fc19c92978bca30b09358c0f27
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-04-08 20:26:27 +0300

    Advance versions

--- version.gradle.kts
+++ version.gradle.kts
@@ -40,13 +40,13 @@
 /**
  * Version of this library.
  */
-val coreJava = "2.0.0-SNAPSHOT.9"
+val coreJava = "2.0.0-SNAPSHOT.14"
 
 /**
  * Versions of the Spine libraries that `core-java` depends on.
  */
 val base = "2.0.0-SNAPSHOT.14"
-val time = "2.0.0-SNAPSHOT.5"
+val time = "2.0.0-SNAPSHOT.14"
 
 project.extra.apply {
     this["versionToPublish"] = coreJava
