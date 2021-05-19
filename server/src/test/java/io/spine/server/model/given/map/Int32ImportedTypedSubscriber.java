04244c451c777027202739eea1577e65b1946fab
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-05-19 15:14:32 +0300

    Add a test for excluding bridge methods

---
+++ server/src/test/java/io/spine/server/model/given/map/Int32ImportedTypedSubscriber.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.model.given.map;
+
+import io.spine.core.Subscribe;
+import io.spine.test.projection.event.Int32Imported;
+
+/**
+ * A typed subscriber to {@code Int32Imported} events.
+ *
+ * <p>By implementing {@code on(Int32Imported)}, this class causes the compiler to generate a bridge
+ * method {@code on(EventMessage)}. The bridge will have the same annotations as the handcrafted
+ * method.
+ */
+public class Int32ImportedTypedSubscriber extends TypedSubscriber<Int32Imported> {
+
+    @Subscribe
+    @Override
+    void on(Int32Imported event) {
+        // Do nothing.
+    }
+}
