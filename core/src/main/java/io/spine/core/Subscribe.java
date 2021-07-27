7781d8bd66f71456451a1596edfb2e21cff493f2
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-07-27 14:45:28 +0300

    Restore `AcceptsFilters`

--- core/src/main/java/io/spine/core/Subscribe.java
+++ core/src/main/java/io/spine/core/Subscribe.java
@@ -105,6 +105,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target(METHOD)
 @Documented
+@AcceptsFilters
 @AcceptsExternal
 public @interface Subscribe {
 }
