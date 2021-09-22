5658d15e0d943ec87795ccfe1e33bc02df2cfe4c
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-09-22 13:43:26 +0300

    Bump the version.

--- version.gradle.kts
+++ version.gradle.kts
@@ -40,7 +40,7 @@
 /**
  * Version of this library.
  */
-val coreJava = "2.0.0-SNAPSHOT.58"
+val coreJava = "2.0.0-SNAPSHOT.59"
 
 /**
  * Versions of the Spine libraries that `core-java` depends on.
