212358f2cdb9a9344cba42f85eb7c17ab3ac457f
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-11-10 16:15:54 +0200

    Update base

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,11 +25,11 @@
  */
 
 /** Versions of the Spine libraries that `core-java` depends on. */
-val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.74")
+val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.75")
 val spineBaseTypesVersion: String by extra("2.0.0-SNAPSHOT.74")
-val spineTimeVersion: String by extra("2.0.0-SNAPSHOT.74")
+val spineTimeVersion: String by extra("2.0.0-SNAPSHOT.75")
 val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.74")
 val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.74")
 
 /** The version of this library. */
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.74")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.75")
