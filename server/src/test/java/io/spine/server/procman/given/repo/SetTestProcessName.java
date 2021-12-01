4980cdf6f40298add2355ea092818018c063a07a
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-12-01 19:14:30 +0200

    Compactify the code.

--- server/src/test/java/io/spine/server/procman/given/repo/SetTestProcessName.java
+++ server/src/test/java/io/spine/server/procman/given/repo/SetTestProcessName.java
@@ -40,8 +40,7 @@ public final class SetTestProcessName
 
     @Override
     public Project apply(Project project) {
-        var newState = project
-                .toBuilder()
+        var newState = project.toBuilder()
                 .clear()
                 .setId(id())
                 .setName(NEW_NAME)
