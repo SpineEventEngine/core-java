87e469bb2c0ed4889e9db555d9c288feff063317
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2021-12-17 17:08:40 +0200

    Update `Aggregate` in compliance to `CommandAssignee`

--- server/src/main/java/io/spine/server/aggregate/Aggregate.java
+++ server/src/main/java/io/spine/server/aggregate/Aggregate.java
@@ -34,7 +34,7 @@ import io.spine.core.Event;
 import io.spine.core.Version;
 import io.spine.protobuf.AnyPacker;
 import io.spine.server.aggregate.model.AggregateClass;
-import io.spine.server.command.CommandHandlingEntity;
+import io.spine.server.command.CommandAssigneeEntity;
 import io.spine.server.dispatch.BatchDispatchOutcome;
 import io.spine.server.dispatch.DispatchOutcome;
 import io.spine.server.entity.EventPlayer;
@@ -82,12 +82,15 @@ import static io.spine.server.aggregate.model.AggregateClass.asAggregateClass;
  *         state types as generic parameters.
  * </ol>
  *
- * <h2>Adding command handler methods</h2>
+ * <h2>Adding command assignee methods</h2>
  *
- * <p>Command handling methods of an {@code Aggregate} are defined in
- * the same way as described in {@link CommandHandlingEntity}.
+ * <p>A command assignee method is a method that is responsible
+ * for handling commands of the specified type.
  *
- * <p>Event(s) returned by command handling methods are posted to
+ * <p>Command assignee methods of an {@code Aggregate} are defined
+ * in the way as it described in {@link io.spine.server.command.Assign Assign}.
+ *
+ * <p>Event(s) returned by command assignees are posted to
  * the {@link io.spine.server.event.EventBus EventBus} automatically
  * by {@link AggregateRepository}.
  *
@@ -124,12 +127,14 @@ import static io.spine.server.aggregate.model.AggregateClass.asAggregateClass;
 public abstract class Aggregate<I,
                                 S extends EntityState<I>,
                                 B extends ValidatingBuilder<S>>
-        extends CommandHandlingEntity<I, S, B>
+        extends CommandAssigneeEntity<I, S, B>
         implements EventPlayer, EventReactor, HasLifecycleColumns<I, S> {
 
     private final UncommittedHistory uncommittedHistory = new UncommittedHistory(this::toSnapshot);
 
-    /** A guard for ensuring idempotency of messages dispatched by this aggregate. */
+    /**
+     * A guard for ensuring idempotency of messages dispatched by this aggregate.
+     */
     private IdempotencyGuard idempotencyGuard;
 
     /**
@@ -139,7 +144,7 @@ public abstract class Aggregate<I,
      *         level because of the following reasons:
      *         <ol>
      *           <li>These constructors are not public API of an application.
-     *           Commands and aggregate IDs are.
+     *                Commands and aggregate IDs are.
      *           <li>These constructors need to be accessible from tests in the same package.
      *         </ol>
      *
@@ -162,7 +167,7 @@ public abstract class Aggregate<I,
      *         level because of the following reasons:
      *         <ol>
      *           <li>These constructors are not public API of an application.
-     *           Commands and aggregate IDs are.
+     *               Commands and aggregate IDs are.
      *           <li>These constructors need to be accessible from tests in the same package.
      *         </ol>
      *
