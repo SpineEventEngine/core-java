2b8857457326441194903bbee775c2ae4f0d7346
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2022-01-04 17:00:15 +0200

    Create `package-info.java`

---
+++ server/src/test/java/io/spine/server/aggregate/given/salary/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Test environment classes for the {@code Aggregate} tests.
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package io.spine.server.aggregate.given.salary;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+
+import javax.annotation.ParametersAreNonnullByDefault;
