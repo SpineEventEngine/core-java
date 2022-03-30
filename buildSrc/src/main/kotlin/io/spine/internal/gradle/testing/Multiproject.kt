c991f2e4942c9981a684d3a52793524152d04dcf
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2022-03-30 17:09:23 +0300

    Expose test only within `java` plugin

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/testing/Multiproject.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/testing/Multiproject.kt
@@ -26,7 +26,9 @@
 
 package io.spine.internal.gradle.testing
 
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.plugins.AppliedPlugin
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.getByType
@@ -55,8 +57,7 @@ import org.gradle.kotlin.dsl.register
  * }
  * ```
  */
-fun Project.exposeTestConfiguration() {
-
+fun Project.exposeTestConfiguration() = pluginManager.withPlugin("java") {
     val testArtifacts = configurations.create("testArtifacts") {
         extendsFrom(configurations.getByName("testRuntimeClasspath"))
     }
