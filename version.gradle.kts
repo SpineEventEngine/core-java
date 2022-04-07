3da8cab46c6b8700b49a98ae4e98733e3095215d
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2022-04-07 19:45:35 +0300

    Rollback ErrorProne and mc-java versions

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,7 +29,7 @@ val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.88")
 val spineBaseTypesVersion: String by extra("2.0.0-SNAPSHOT.88")
 val spineTimeVersion: String by extra("2.0.0-SNAPSHOT.88")
 val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.88")
-val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.90")
+val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.83")
 
 /** The version of this library. */
 val versionToPublish: String by extra("2.0.0-SNAPSHOT.94")
