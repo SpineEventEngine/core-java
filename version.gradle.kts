169db76e642d9190b4d63e26eeeb88942f7c952e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-08-29 16:59:43 +0300

    Bump base -> `2.0.0-SNAPSHOT.94`

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,7 +25,7 @@
  */
 
 /** Versions of the Spine libraries that `core-java` depends on. */
-val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.93")
+val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.94")
 val spineBaseTypesVersion: String by extra("2.0.0-SNAPSHOT.88")
 val spineTimeVersion: String by extra("2.0.0-SNAPSHOT.88")
 val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.83")
