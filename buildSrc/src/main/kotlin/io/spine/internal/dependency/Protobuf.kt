d365816e6666b036b3a8c930ad502942ce902453
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-09-29 21:31:40 +0300

    Bump Protobuf -> `3.12.6`, Gradle Plugin -> `0.8.19`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
@@ -30,7 +30,7 @@ package io.spine.internal.dependency
 @Suppress("MemberVisibilityCanBePrivate") // used directly from outside
 object Protobuf {
     private const val group = "com.google.protobuf"
-    const val version       = "3.20.1"
+    const val version       = "3.21.6"
     val libs = listOf(
         "${group}:protobuf-java:${version}",
         "${group}:protobuf-java-util:${version}",
@@ -46,7 +46,7 @@ object Protobuf {
          *
          * When changing the version, also change the version used in the `build.gradle.kts`.
          */
-        const val version = "0.8.18"
+        const val version = "0.8.19"
         const val id = "com.google.protobuf"
         const val lib = "${group}:protobuf-gradle-plugin:${version}"
     }
