f0b35da50fe5af6f7b31af62fbf55f29315818d5
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-17 21:04:37 +0000

    Extend `BlackBox` with inline fun
    ... for creating `EntitySubject`.

---
+++ testutil-server/src/main/kotlin/io/spine/server/testing/blackbox/BlackBoxExts.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.server.testing.blackbox
+
+import io.spine.server.entity.Entity
+import io.spine.testing.server.blackbox.BlackBox
+import io.spine.testing.server.entity.EntitySubject
+
+/**
+ * Creates [EntitySubject] for the entity of the type [E] with the given ID of type [I].
+ */
+public inline fun <reified E: Entity<I, *>, I> BlackBox.assertEntity(id: I): EntitySubject =
+    assertEntity(id, E::class.java)
