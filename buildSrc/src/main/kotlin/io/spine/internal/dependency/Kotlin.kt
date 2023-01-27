de29910675c9e2c84700b33814ff6235f3f0a741
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-27 14:22:33 +0000

    Bump Kotlin -> `1.8.0`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
@@ -35,7 +35,7 @@ object Kotlin {
      * When changing the version, also change the version used in the `buildSrc/build.gradle.kts`.
      */
     @Suppress("MemberVisibilityCanBePrivate") // used directly from outside
-    const val version = "1.7.21"
+    const val version = "1.8.0"
 
     private const val group = "org.jetbrains.kotlin"
 
