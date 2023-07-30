ed075ae7cfc384c1f8a09d26bf37c5c1e6d4b2ed
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-30 21:04:58 +0100

    Bump `mc-java` -> `2.0.0-SNAPSHOT.166`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -89,7 +89,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/mc-java">spine-mc-java</a>
          */
-        const val mcJava = "2.0.0-SNAPSHOT.165"
+        const val mcJava = "2.0.0-SNAPSHOT.166"
 
         /**
          * The version of [Spine.baseTypes].
