74128ef30afd734b84544167e53f089878062309
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-24 15:40:51 +0000

    Bump version -> `2.0.0-SNAPSHOT.182`

--- version.gradle.kts
+++ version.gradle.kts
@@ -27,6 +27,6 @@
 /**
  *  The version of this library.
  *
- * For versions of Spine-based dependencies, please see [io.spine.internal.dependency.Spine].
+ * For versions of Spine-based dependencies, please see [io.spine.dependency.local.Spine].
  */
 val versionToPublish: String by extra("2.0.0-SNAPSHOT.182")
