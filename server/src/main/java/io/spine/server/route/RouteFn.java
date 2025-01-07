8d05a33e0bc9d64e0c3e23ec3233abc94b028bd1
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-07 21:19:38 +0000

    Do not make `RouteFn` serializable

--- server/src/main/java/io/spine/server/route/RouteFn.java
+++ server/src/main/java/io/spine/server/route/RouteFn.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -28,7 +28,6 @@ package io.spine.server.route;
 
 import com.google.protobuf.Message;
 
-import java.io.Serializable;
 import java.util.function.BiFunction;
 
 /**
@@ -43,7 +42,7 @@ import java.util.function.BiFunction;
  */
 @FunctionalInterface
 public interface RouteFn<M extends Message, C extends Message, R>
-        extends BiFunction<M, C, R>, Serializable {
+        extends BiFunction<M, C, R> {
 
     /**
      * Obtains entity ID(s) from the passed message and its context.
@@ -57,6 +56,7 @@ public interface RouteFn<M extends Message, C extends Message, R>
      *         Javadoc references. Without overriding it will be {@code #apply(Object, Object)}
      *         which may be confusing in the context of event routing.
      */
+    @SuppressWarnings("AbstractMethodOverridesAbstractMethod") // for documentation.
     @Override
     R apply(M message, C context);
 }
