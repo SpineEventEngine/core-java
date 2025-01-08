69440fdd2902efc2991021d584d9922cb52338be
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-08 18:23:23 +0000

    Bump Validation -> `2.0.0-SNAPSHOT.186`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
@@ -36,7 +36,7 @@ object Validation {
     /**
      * The version of the Validation library artifacts.
      */
-    const val version = "2.0.0-SNAPSHOT.183"
+    const val version = "2.0.0-SNAPSHOT.186"
 
     const val group = "io.spine.validation"
     private const val prefix = "spine-validation"
