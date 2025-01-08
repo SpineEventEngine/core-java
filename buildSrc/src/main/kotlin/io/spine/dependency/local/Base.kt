105ef8045e541921842445522f31e9392af8b20b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-08 16:50:20 +0000

    Bump Base -> `2.0.0-SNAPSHOT.236`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Base.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Base.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,8 +33,8 @@ package io.spine.dependency.local
  */
 @Suppress("ConstPropertyName")
 object Base {
-    const val version = "2.0.0-SNAPSHOT.232"
-    const val versionForBuildScript = "2.0.0-SNAPSHOT.232"
+    const val version = "2.0.0-SNAPSHOT.236"
+    const val versionForBuildScript = "2.0.0-SNAPSHOT.236"
     const val group = Spine.group
     const val artifact = "spine-base"
     const val lib = "$group:$artifact:$version"
