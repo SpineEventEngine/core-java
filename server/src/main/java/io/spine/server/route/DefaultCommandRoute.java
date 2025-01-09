354dda7abd8c3d280b123667620ce1ea4fe44758
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-09 14:41:07 +0000

    Remove unused field

--- server/src/main/java/io/spine/server/route/DefaultCommandRoute.java
+++ server/src/main/java/io/spine/server/route/DefaultCommandRoute.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -38,8 +38,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @param <I> the type of target entity IDs
  */
 public final class DefaultCommandRoute<I> implements CommandRoute<I, CommandMessage> {
-
-    private static final long serialVersionUID = 0L;
 
     private final FirstField<I, CommandMessage, CommandContext> field;
 
