119267c7797caf1d81395edf8a0db8947df46714
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-09 18:49:42 +0000

    Remove redundant `serialVersionUID`

--- server/src/main/java/io/spine/server/entity/model/AssigneeEntityClass.java
+++ server/src/main/java/io/spine/server/entity/model/AssigneeEntityClass.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -49,7 +49,6 @@ public abstract class AssigneeEntityClass<E extends Entity<?, ?>>
         extends EntityClass<E>
         implements CommandHandlingClass<EventClass, AssigneeReceptor> {
 
-    private static final long serialVersionUID = 0L;
     private final ReceptorMap<CommandClass, EventClass, AssigneeReceptor> commands;
 
     protected AssigneeEntityClass(Class<E> cls) {
