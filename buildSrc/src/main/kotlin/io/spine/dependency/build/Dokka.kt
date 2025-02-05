d27098f7058ed5b9dd94da0349e186e0a37f6a59
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-02-05 19:55:46 +0000

    Bump our Dokka extensions -> `2.0.0-SNAPSHOT.6`

--- buildSrc/src/main/kotlin/io/spine/dependency/build/Dokka.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/build/Dokka.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,7 +78,7 @@ object Dokka {
     object SpineExtensions {
         private const val group = "io.spine.tools"
 
-        const val version = "2.0.0-SNAPSHOT.4"
+        const val version = "2.0.0-SNAPSHOT.6"
         const val lib = "$group:spine-dokka-extensions:$version"
     }
 }
