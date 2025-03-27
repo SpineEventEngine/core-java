3c233519d221051133ffebc1a9de9c502dae3736
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-27 15:58:07 +0000

    Bump McJava -> `2.0.0-SNAPSHOT.307`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/McJava.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/McJava.kt
@@ -42,12 +42,12 @@ object McJava {
     /**
      * The version used to in the build classpath.
      */
-    const val dogfoodingVersion = "2.0.0-SNAPSHOT.306"
+    const val dogfoodingVersion = "2.0.0-SNAPSHOT.307"
 
     /**
      * The version to be used for integration tests.
      */
-    const val version = "2.0.0-SNAPSHOT.306"
+    const val version = "2.0.0-SNAPSHOT.307"
 
     /**
      * The ID of the Gradle plugin.
