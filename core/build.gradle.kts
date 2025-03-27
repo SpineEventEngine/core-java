cf7bd28e6c58b68c3f03481a4f0d70029529ecd8
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-27 13:59:03 +0000

    Fix missing import

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import io.spine.dependency.lib.Coroutines
 import io.spine.dependency.lib.KotlinX
 import io.spine.dependency.local.Base
 import io.spine.dependency.local.BaseTypes
