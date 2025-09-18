cb2fd1b91c90df67b23ea11f06653e7429ffc942
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-09-18 17:43:15 +0100

    Remove API level annotations

--- server/src/main/java/io/spine/server/entity/status/package-info.java
+++ server/src/main/java/io/spine/server/entity/status/package-info.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -27,13 +27,9 @@
 /**
  * This package contains classes and interfaces for working with entity statuses.
  */
-
-@SPI
 @CheckReturnValue
 @NullMarked
 package io.spine.server.entity.status;
 
 import com.google.errorprone.annotations.CheckReturnValue;
-import io.spine.annotation.SPI;
-
 import org.jspecify.annotations.NullMarked;
